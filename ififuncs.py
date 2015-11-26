82f5332826dbac4fb88f5ff905084a76d70bf783
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-26 12:09:10 +0000

    added qctools creation - gzip - output not yet there

--- ififuncs.py
+++ ififuncs.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 def make_mediainfo(xmlfilename, xmlvariable, inputfilename):
   with open(xmlfilename, "w+") as fo:
@@ -8,3 +9,14 @@ def make_mediainfo(xmlfilename, xmlvariable, inputfilename):
   						'--output=XML',
   						inputfilename])       #input filename
   	fo.write(xmlvariable)
+
+
+def make_qctools(input):
+    qctools_args = ['ffprobe', '-f', 'lavfi', '-i',] 
+    qctools_args += ["movie=%s:s=v+a[in0][in1],[in0]signalstats=stat=tout+vrep+brng,cropdetect=reset=1,split[a][b];[a]field=top[a1];[b]field=bottom[b1],[a1][b1]psnr[out0];[in1]ebur128=metadata=1[out1]" % input] 
+    qctools_args += ['-show_frames', '-show_versions', '-of', 'xml=x=1:q=1', '-noprivate']
+    print qctools_args
+    subprocess.call(qctools_args)
+    #"movie=%s:s=v+a[in0][in1],[in0]signalstats=stat=tout+vrep+brng,cropdetect=reset=1,split[a][b];[a]field=top[a1];[b]field=bottom[b1],[a1][b1]psnr[out0];[in1]ebur128=metadata=1[out1]", 
+    #'-show_frames', '-show_versions', '-of', 'xml=x=1:q=1', '-noprivate', % input]) 
+    
