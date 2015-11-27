fd9d39459b7612c141415f03acc8d04b82e1d741
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-27 12:10:18 +0000

    updated make_qctools

--- ififuncs.py
+++ ififuncs.py
@@ -12,13 +12,13 @@ def make_mediainfo(xmlfilename, xmlvariable, inputfilename):
 
 
 def make_qctools(input):
+   
     qctools_args = ['ffprobe', '-f', 'lavfi', '-i',] 
-    qctools_args += ["movie=%s:s=v+a[in0][in1],[in0]signalstats=stat=tout+vrep+brng,cropdetect=reset=1,split[a][b];[a]field=top[a1];[b]field=bottom[b1],[a1][b1]psnr[out0];[in1]ebur128=metadata=1[out1]" % input] 
+    qctools_args += ["movie=%s:s=v+a[in0][in1],[in0]signalstats=stat=tout+vrep+brng,cropdetect=reset=1:round=1,split[a][b];[a]field=top[a1];[b]field=bottom[b1],[a1][b1]psnr[out0];[in1]ebur128=metadata=1,astats=metadata=1:reset=1:length=0.4[out1]" % input] 
     qctools_args += ['-show_frames', '-show_versions', '-of', 'xml=x=1:q=1', '-noprivate']
     print qctools_args
-    subprocess.call(qctools_args)
-    #"movie=%s:s=v+a[in0][in1],[in0]signalstats=stat=tout+vrep+brng,cropdetect=reset=1,split[a][b];[a]field=top[a1];[b]field=bottom[b1],[a1][b1]psnr[out0];[in1]ebur128=metadata=1[out1]", 
-    #'-show_frames', '-show_versions', '-of', 'xml=x=1:q=1', '-noprivate', % input]) 
+    qctoolsreport = subprocess.check_output(qctools_args)
+    return qctoolsreport
    
 def write_qctools_gz(qctoolsxml, sourcefile):
     with open(qctoolsxml, "w+") as fo:
