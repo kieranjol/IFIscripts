100f2affecec3ea884c8b8966ddbe07ed2b34f07
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-04-19 20:20:31 +0100

    prores.py - remove debug info and cleanup

--- prores.py
+++ prores.py
@@ -7,9 +7,8 @@ import os
 from glob import glob
 import pdb
 
-
-parser = argparse.ArgumentParser(description="test")
-parser.add_argument('input')
+parser = argparse.ArgumentParser(description="IFI Apple Pro Res 4:2:2 ffmpeg Encoder. Written by Kieran O'Leary.")
+parser.add_argument('Filename to process one file, or foldername to process a batch. ')
 parser.add_argument('-hq', action='store_true',help='-profile:v 3 aka Prores HQ')
 parser.add_argument('-yadif', action='store_true',help='Yet Another DeInterlace Filter')
 parser.add_argument('-scale',help='Rescale video. Usage: -scale 1920x1080 or -scale 720x576')
@@ -17,29 +16,18 @@ parser.add_argument('-md5', action='store_true',help='Get md5 sidecar for your o
 
 args = parser.parse_args()
 prores_options = []
-
-if args.hq:
-    print 'Prores HQ'
     
 if args.yadif:
-    print 'yadif'
     prores_options.append('-yadif')
+    
 if args.scale:
-    print 'rescale'
     prores_options.append('-scale')
     width_height = args.scale
-print prores_options
-number_of_effects =  len(prores_options)
-if not prores_options:
-    print 'Processing your prores with no extra options'
-else:    
-    prores_options.insert(0, '-vf')
 
-print args.input
+number_of_effects =  len(prores_options)
 
  # Input, either file or firectory, that we want to process.
 input = args.input
-print input
 
 # Store the directory containing the input file/directory.
 wd = os.path.dirname(input)
@@ -49,18 +37,12 @@ os.chdir(wd)
 
 # Store the actual file/directory name without the full path.
 file_without_path = os.path.basename(input)
-print file_without_path
-
-
 
 # Check if input is a file.
 # AFAIK, os.path.isfile only works if full path isn't present.
 if os.path.isfile(file_without_path):      
-    print os.path.isfile(file_without_path)
-    print "single file found"
     video_files = []                       # Create empty list 
     video_files.append(file_without_path)  # Add filename to list
-    print video_files
 
 # Check if input is a directory. 
 elif os.path.isdir(file_without_path):  
@@ -70,13 +52,10 @@ elif os.path.isdir(file_without_path):
 # Prints some stuff if input isn't a file or directory.
 else: 
     print "Your input isn't a file or a directory."
-    print "What was it? I'm curious."  
-print prores_options 
-
                      
 for filename in video_files:
             #pdb.set_trace()
-            output = filename + "ffmpeg.mov"
+            output = filename + "_prores.mov"
             
             ffmpeg_args =   ['ffmpeg',
                     '-i', filename,
@@ -84,29 +63,24 @@ for filename in video_files:
             if args.hq:
                 ffmpeg_args.append('-profile:v')
                 ffmpeg_args.append('3')
+                
             if args.yadif or args.scale:
                 
-                optio = '-vf'
+                filter_options = '-vf'
                 
                 if args.yadif:
                     print 'YADIF'
-                    optio += ' yadif'
-                       
+                    filter_options += ' yadif'         
                       
                 if args.scale:
                     print 'scale'
                     if number_of_effects > 1:
-                    
-                        optio += (',scale=%s' % width_height)
+                        filter_options += (',scale=%s' % width_height)                        
                     else:
-                        optio += (' scale=%s' % width_height)
+                        filter_options += (' scale=%s' % width_height)
                     
-                    
-                optio = optio.split()
-                print optio
-                for item in optio:
-                
+                filter_options = filter_options.split()
+                for item in filter_options:
                     ffmpeg_args.append(item)    
             ffmpeg_args.append(output)
-            print ffmpeg_args
             subprocess.call(ffmpeg_args)
