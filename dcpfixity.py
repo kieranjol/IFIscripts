704963758ae9599a046a8f2c024dbb9321e6993f
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-04-23 20:01:14 +0100

    dcpfixity - better variables, lots of changes

--- dcpfixity.py
+++ dcpfixity.py
@@ -4,29 +4,69 @@ from glob import glob
 import os
 from os import listdir
 from os.path import isfile, join
+import subprocess
+import base64
 
-myfile = sys.argv[1]
-#os.chdir(myfile)
-onlyfiles = [f for f in listdir(myfile) if isfile(join(myfile, f))]
-    
-print onlyfiles
-if 'ASSETMAP' in onlyfiles:
+dcp_dir = sys.argv[1]
+os.chdir(dcp_dir)
+dcp_files = [f for f in listdir(dcp_dir) if isfile(join(dcp_dir, f))]
+if 'ASSETMAP' in dcp_files:
     print 'interop'
-elif 'ASSETMAP.xml' in onlyfiles:
-    print 'smpte'
-doc = etree.parse(myfile)
-root = doc.getroot()
-#print doc.xpath('//ns:Path',namespaces={'ns': "http://www.smpte-ra.org/schemas/429-9/2007/AM"})
+    assetmap = 'ASSETMAP'
+elif 'ASSETMAP.xml' in dcp_files:
+    assetmap = 'ASSETMAP.xml'
+assetmap_xml = etree.parse(assetmap)
+assetmap_namespace = assetmap_xml.xpath('namespace-uri(.)')
+root = assetmap_xml.getroot()   
+#print dcp_files
+xmlfiles = glob('*.xml')
+#print xmlfiles
+for i in xmlfiles:
+    xmlname = etree.parse(i)
+    is_pkl = xmlname.xpath('namespace-uri(.)')
+    if 'PKL' in is_pkl:
+        print  str(is_pkl) + 'blablablba'
+        pkl_file = i
+        print 'pkl_file is ' + pkl_file
+    
+pkl_parse = etree.parse(pkl_file)
+pkl_namespace = pkl_parse.xpath('namespace-uri(.)') 
+print pkl_namespace  
+hashes =  pkl_parse.findall('//ns:Hash',namespaces={'ns': pkl_namespace})
 
-var =  doc.findall('//ns:Path',namespaces={'ns': "http://www.smpte-ra.org/schemas/429-9/2007/AM"})
-xmlid =  doc.findall('//ns:Asset/ns:Id',namespaces={'ns': "http://www.smpte-ra.org/schemas/429-9/2007/AM"})
-print len(var) - 1
+xmluuid =  pkl_parse.findall('//ns:Asset/ns:Id',namespaces={'ns': pkl_namespace})
+#print len(var) - 1
+counter = 0
+pkl_hashes = {}
+while counter <= len(hashes) -1 :
+    #print var[counter].text
+    pkl_hashes[xmluuid[counter].text] = hashes[counter].text # {pkl_uuid:pkl_hash}
+    counter +=1
+#print assetmap_xml.xpath('//ns:Path',namespaces={'ns': "http://www.smpte-ra.org/schemas/429-9/2007/AM"})
+print pkl_hashes
+var =  assetmap_xml.findall('//ns:Path',namespaces={'ns': assetmap_namespace})
+
+xmlid =  assetmap_xml.findall('//ns:Asset/ns:Id',namespaces={'ns': assetmap_namespace})
+#print len(var) - 1
 counter = 0
 file_paths = {}
 while counter <= len(var) -1 :
-    print var[counter].text
-    file_paths[var[counter].text] = xmlid[counter].text
+    #print var[counter].text
+    file_paths[xmlid[counter].text] = [var[counter].text] # {assetmapuuid:assetmapfilename}
     counter +=1
+print file_paths
+for i in file_paths:
+    print file_paths[i][0]
+
+    openssl_hash = subprocess.check_output(['openssl', 'sha1', '-binary', file_paths[i][0]])
+    b64hash =  base64.b64encode(openssl_hash)
+    print b64hash  
+    file_paths[i].append(b64hash)
+print file_paths
+    
+# get real hashes
+
+
 '''for i in xmlid:
     print i.text
 print var[7].text
@@ -34,13 +74,13 @@ print var[7].text
 #print xmlid
 
 
-print file_paths
-'''for elt in doc.getiterator():
+#print file_paths
+'''for elt in assetmap_xml.getiterator():
    
     print elt.tag
 '''
 '''
-f = etree.parse(myfile)
+f = etree.parse(dcp_dir)
 root = f.getroot()
 namespace = f.xpath('namespace-uri(.)')
 
