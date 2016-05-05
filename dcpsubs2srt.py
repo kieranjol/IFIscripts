bea69d7ba8fbe5311bc7459bef69b92192c97f47
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-05-05 14:11:06 +0100

    dcpsubs2srt.py - cleanup and better variables

--- dcpsubs2srt.py
+++ dcpsubs2srt.py
@@ -4,28 +4,26 @@ from lxml import etree
 filename = sys.argv[1]
 srt_file = filename +'.srt'
 
-xmlo = etree.parse(filename)
-count = int(xmlo.xpath('count(//Subtitle)'))
+dcp_subtitle = etree.parse(filename)
+total_subtitles = int(dcp_subtitle.xpath('count(//Subtitle)'))
 counter = 0
 
 with open(srt_file, "w") as myfile:
-       print 'Transforming ', count, 'subtitles'
+       print  srt_file,' created'
 
-while counter < count:
-    counter2 = counter +1
-    in_point = xmlo.xpath('//Subtitle')[counter].attrib['TimeIn']
-    out = xmlo.xpath('//Subtitle')[counter].attrib['TimeOut']
-    in_point = in_point[:8] + '.' + in_point[9:]
-    out = out[:8] + '.' + out[9:]
+while counter < total_subtitles:
+    xpath_counter = counter + 1
+    in_point      = dcp_subtitle.xpath('//Subtitle')[counter].attrib['TimeIn']
+    out_point     = dcp_subtitle.xpath('//Subtitle')[counter].attrib['TimeOut']
+    in_point      = in_point[:8]  + '.' + in_point[9:]
+    out_point     = out_point[:8] + '.' + out_point[9:]
 
     with open(srt_file, "a") as myfile:
         myfile.write(str(counter + 1) + '\n')
-        myfile.write(in_point + ' --> ' + out + '\n')
-        bla =  [bla.text for bla in xmlo.iterfind('.//Subtitle[%s]/Text' % int(counter2) ) ]
-        for i in bla:
+        myfile.write(in_point + ' --> ' + out_point + '\n')
+        subtitle_text =  [subtitle_text.text for subtitle_text in dcp_subtitle.iterfind('.//Subtitle[%s]/Text' % int(xpath_counter) ) ]
+        for i in subtitle_text:
                 myfile.write(i + '\n')
         myfile.write('\n')
-
-        print 'Transforming ' + str(counter) + ' of' + str(count) + ' subtitles\r' ,
-          
+        
     counter +=1 
