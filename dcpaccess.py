4e7576f09aeaafb643430df2646f5b4fcc5f3611
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-05-05 09:12:07 +0100

    dcpaccess - add dcp_subs_xml to SRT

--- dcpaccess.py
+++ dcpaccess.py
@@ -220,7 +220,36 @@ for root,dirnames,filenames in os.walk(dcp_dir):
             counter = 0
             count = len(subs)
             while counter <= count:
-                command = ['ffmpeg','-i',pic_mxfs[counter],'-i',aud_mxfs[counter],'-c:a','copy', '-c:v', 'libx264', '-vf', 'subtitle=%s' % subs[counter]  + '.mkv' ]
+                srt_file = subs[counter] +'.srt'
+
+                xmlo = etree.parse(subs[counter])
+                count = int(xmlo.xpath('count(//Subtitle)'))
+                
+
+                with open(srt_file, "w") as myfile:
+                       print 'Transforming ', count, 'subtitles'
+
+                while counter < count:
+                    counter2 = counter +1
+                    in_point = xmlo.xpath('//Subtitle')[counter].attrib['TimeIn']
+                    out = xmlo.xpath('//Subtitle')[counter].attrib['TimeOut']
+                    in_point = in_point[:8] + '.' + in_point[9:]
+                    out = out[:8] + '.' + out[9:]
+
+                    with open(srt_file, "a") as myfile:
+                        myfile.write(str(counter + 1) + '\n')
+                        myfile.write(in_point + ' --> ' + out + '\n')
+                        bla =  [bla.text for bla in xmlo.iterfind('.//Subtitle[%s]/Text' % int(counter2) ) ]
+                        for i in bla:
+                                myfile.write(i + '\n')
+                        myfile.write('\n')
+
+                        print 'Transforming ' + str(counter) + ' of' + str(count) + ' subtitles\r' ,
+                          
+                    counter +=1 
+                counter = 0
+                count = len(subs)
+                command = ['ffmpeg','-i',pic_mxfs[counter],'-i',aud_mxfs[counter],'-c:a','copy', '-c:v', 'libx264', '-vf', 'subtitles=%s' % srt_file, 'output.mkv' ]
                 print command
                 subprocess.call(command)
                 counter +=1 
