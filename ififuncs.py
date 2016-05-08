96fcda28bcee761c72e6de8b64564834f8d1ef70
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-05-08 15:20:54 +0100

    ififuncs - add emailer and frames2seconds

--- ififuncs.py
+++ ififuncs.py
@@ -1,5 +1,16 @@
 import subprocess
 import sys
+import base64
+import time
+import smtplib
+import mimetypes
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+from email.message import Message
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
 
 def make_mediainfo(xmlfilename, xmlvariable, inputfilename):
   with open(xmlfilename, "w+") as fo:
@@ -32,3 +43,49 @@ def get_mediainfo(var_type, type, filename):
     var_type = subprocess.check_output(['MediaInfo', '--Language=raw', '--Full', type , filename ]).replace('\n', '')
     return var_type
 # example - duration =  get_mediainfo('duration', '--inform=General;%Duration_String4%', sys.argv[1] )
+
+def send_gmail(email_to, attachment, subject, email_body):
+    emailfrom = ""
+    emailto = email_to
+    #emailto = ", ".join(emailto)
+    fileToSend = attachment
+    username = ""
+    password = ""
+
+    msg = MIMEMultipart()
+    msg["From"]    = emailfrom
+    msg["To"]      = ", ".join(emailto)
+    msg["Subject"] = subject
+    msg.preamble   = "testtesttest"
+    body = MIMEText(email_body)
+    msg.attach(body)
+
+    ctype, encoding = mimetypes.guess_type(fileToSend)
+    if ctype is None or encoding is not None:
+        ctype = "application/octet-stream"
+
+    maintype, subtype = ctype.split("/", 1)
+
+    if maintype == "text":
+        fp = open(fileToSend)
+        # Note: we should handle calculating the charset
+        attachment = MIMEText(fp.read(), _subtype=subtype)
+        fp.close()
+    elif maintype == "image":
+        fp = open(fileToSend, "rb")
+        attachment = MIMEImage(fp.read(), _subtype=subtype)
+        fp.close()
+    elif maintype == "audio":
+        fp = open(fileToSend, "rb")
+        attachment = MIMEAudio(fp.read(), _subtype=subtype)
+        fp.close()
+    else:
+        fp = open(fileToSend, "rb")
+        attachment = MIMEBase(maintype, subtype)
+        attachment.set_payload(fp.read())
+        
+def frames_to_seconds(audio_entry_point):
+    audio_frame_count  = float(audio_entry_point) 
+    audio_frame_count  = float(audio_frame_count) / 24.000 # Change to EditRate variable.
+    audio_frame_count  = round(audio_frame_count, 3)
+    return audio_frame_count
