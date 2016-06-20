8d506ae98307ed07f7a65496755343235ca1b7e5
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-06-20 16:42:26 +0100

    move.py add argparse & file/manifest counts

--- move.py
+++ move.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 import subprocess
 import os
@@ -6,19 +8,48 @@ import filecmp
 from sys import platform as _platform
 import tempfile
 import time
+import argparse
 
 # Currently, destination manifest will be overwritten. user input should be required. source manifest will not be overwritten, it will be read.
+parser = argparse.ArgumentParser(description='Copy directory with checksum comparison and manifest generation.'
+                                 ' Written by Kieran O\'Leary.')
+parser.add_argument('source', help='Input directory')
+parser.add_argument('destination', help='Destination directory')
+parser.add_argument('-b', '-benchmark', action='store_true', help='display benchmark')
 
-source               = sys.argv[1]
+args = parser.parse_args()
+
+source               = args.source
 source_parent_dir    = os.path.dirname(source)
 normpath             = os.path.normpath(source) 
 dirname              = os.path.splitext(os.path.basename(source))[0]
 relative_path        = normpath.split(os.sep)[-1]
 
-destination                    = sys.argv[2] # or hardcode
+destination                    = args.destination # or hardcode
 manifest_destination           = destination + '/%s_manifest.md5' % dirname
 destination_final_path         = destination + '/%s' % dirname
 manifest                       = source_parent_dir + '/%s_manifest.md5' % relative_path
+'''
+file_list = []
+
+for root, directories, filenames in os.walk(source):
+    root_list = os.listdir(root)
+    for i in root_list:
+        file_list.append(i)
+    for directory in directories:
+             os.chdir(root)
+             dirlist = os.listdir(directory)
+             for i in dirlist:
+                 file_list.append(i)
+'''
+
+def count_files(location):
+    file_count = 0
+    for dirs,subdir,files in os.walk(location):
+        file_count += len(files)
+        for subdirs in subdir:
+            file_count +=len(files)
+        return file_count
 
 def display_benchmark():
     print 'SOURCE MANIFEST TIME', source_manifest_time
@@ -35,6 +66,7 @@ def test_write_capabilities(directory):
     else:
         print ' %s is either not a directory or it does not exist' % directory
         sys.exit()
+
 def remove_bad_files(root_dir):
     rm_these = ['.DS_Store', 'Thumbs.db', 'desktop.ini']
     for root, dirs, files in os.walk(root_dir):
@@ -51,22 +83,23 @@ def make_manifest(manifest_dir, relative_manifest_path, manifest_textfile):
         print 'Destination manifest already exists'
     manifest_generator = subprocess.check_output(['md5deep', '-ler', relative_manifest_path])
     manifest_list = manifest_generator.splitlines()
+    files_in_manifest = len(manifest_list)
     manifest_list.sort()
     with open(manifest_textfile,"wb") as fo:
         for i in manifest_list:
             fo.write(i + '\n')
+    return files_in_manifest
 
 def copy_dir():
     if _platform == "win32":
-        subprocess.call(['robocopy',sys.argv[1], destination_final_path, '/E'])
+        subprocess.call(['robocopy',source, destination_final_path, '/E'])
     elif _platform == "darwin":
         # https://github.com/amiaopensource/ltopers/blob/master/writelto#L51
-        cmd = ['gcp','--preserve=mode,timestamps', '-nRv',sys.argv[1], destination_final_path]
+        cmd = ['gcp','--preserve=mode,timestamps', '-nRv',source, destination_final_path]
         subprocess.call(cmd)
     elif _platform == "linux2":
         # https://github.com/amiaopensource/ltopers/blob/master/writelto#L51
-        cmd = [ 'cp','--preserve=mode,timestamps', '-nRv',sys.argv[1], destination_final_path]
-        print cmd
+        cmd = [ 'cp','--preserve=mode,timestamps', '-nR',source, destination_final_path]
         subprocess.call(cmd)
 
 def check_overwrite(file2check):
@@ -88,6 +121,7 @@ def check_overwrite_dir(dir2check):
             if overwrite_destination_dir not in ('Y','y','N','n'):
                 print 'Incorrect input. Please enter Y or N'
         return overwrite_destination_dir
+
 try:
     test_write_capabilities(source)
 except OSError:
@@ -102,14 +136,17 @@ overwrite_destination_manifest = check_overwrite(manifest_destination)
 overwrite_destination_dir = check_overwrite_dir(destination_final_path)
 
 remove_bad_files(source)
-
+files_in_source = count_files(source) 
 source_manifest_start_time = time.time()
+
 if not os.path.isfile(manifest):
     try:
+        print 'Generating source manifest'
         make_manifest(source_parent_dir, relative_path,manifest)
     except OSError:
             print 'You do not have access to this directory. Perhaps it is read only, or the wrong file system\n'
             sys.exit()
+
 source_manifest_time = time.time() - source_manifest_start_time
 
 copy_start_time = time.time()
@@ -119,17 +156,17 @@ copy_time = time.time() - copy_start_time
 
 start_destination_manifest_time = time.time()
 if overwrite_destination_manifest not in ('N','n'):
-    make_manifest(sys.argv[2],dirname, manifest_destination)
+    print 'Generating destination manifest'
+    files_in_manifest = make_manifest(destination,dirname, manifest_destination)
 remove_bad_files(destination_final_path)
 
 destination_manifest_time = time.time() - start_destination_manifest_time
-
+files_in_destination = count_files(destination_final_path)
 if filecmp.cmp(manifest, manifest_destination, shallow=False):
 	print "Your files have reached their destination and the checksums match"
 else:
-	print "YOUR CHECKSUMS DO NOT MATCH, BACK TO THE DRAWING BOARD!!!"
-	display_benchmark()
-	sys.exit()
-
-display_benchmark()
+	print "***********YOUR CHECKSUMS DO NOT MATCH*************"
+print ' There are: \n %s files in your destination manifest \n %s files in your destination \n %s files at source' % (files_in_manifest, files_in_destination, files_in_source)
+if args.b:
+    display_benchmark()
 
