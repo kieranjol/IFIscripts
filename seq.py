9e76e0d2aaaf5e0404615f3066399ea992f73f52
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-06-22 12:39:22 +0100

    seq.py - accept directory input rather than file
    As image sequences can take a long time to open in finder/explorer, taking a directory as input can speed things up.

--- seq.py
+++ seq.py
@@ -3,18 +3,28 @@
 import subprocess
 import sys
 import os
+from glob import glob
 
 first_file = sys.argv[1]
-rejigged_name = first_file.split("_")[0:-1]
+os.chdir(first_file)
+images = (
+        glob('*.tif') +
+        glob('*.tiff') +
+        glob('*.dpx') 
+        )
+
+       
+rejigged_name = images[0].split("_")[0:-1]
+print rejigged_name
 test = ''
 counter = 0
 while  counter <len(rejigged_name) :
     test += rejigged_name[counter] + '_'
     counter += 1
 dirname = os.path.dirname(first_file)    
-output = os.path.dirname(dirname) + '/%s_consolidate.mov' % os.path.split(dirname)[-1]
+output = dirname + '/%s_consolidate.mov' % os.path.split(first_file)[-1]
 test += '%06d.tiff'
 cmd = ['ffmpeg','-f','image2','-framerate','24', '-i',test,'-c:v','v210',output]
 print cmd
-subprocess.call(cmd)
 
+subprocess.call(cmd)
