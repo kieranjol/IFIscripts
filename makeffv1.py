ebb02ca33945e705d7f7b2fc8a52cf335bb667bc
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-07-25 23:44:00 +0100

    makeffv1 - basic logging test

--- makeffv1.py
+++ makeffv1.py
@@ -12,6 +12,7 @@ import shutil
 import csv
 import time
 import itertools
+import getpass
 
 def create_csv(csv_file, *args):
     f = open(csv_file, 'wb')
@@ -30,7 +31,13 @@ def append_csv(csv_file, *args):
     finally:
         f.close()
 
-
+def generate_log(log, what2log):
+    if not os.path.isfile(log):
+        with open(log,"wb") as fo:
+            fo.write(time.strftime("%Y-%m-%dT%H:%M:%S ") + getpass.getuser() + ' ' + what2log + ' \n')
+    else:
+        with open(log,"ab") as fo:
+            fo.write(time.strftime("%Y-%m-%dT%H:%M:%S ") + getpass.getuser() + ' ' + what2log + ' \n')
 # Write metadata for original video file - with open will auto close the file.
 def make_mediainfo(xmlfilename, xmlvariable, inputfilename):
     with open(xmlfilename, "w+") as fo:
@@ -98,7 +105,7 @@ else:
         print "What was it? I'm curious."  
     create_csv(csv_report_filename, ('FILENAME', 'Lossless?'))
     for filename in video_files: #loop all files in directory
-    
+
 
         filenoext = os.path.splitext(filename)[0]
         # Change directory to directory with video files
@@ -127,7 +134,10 @@ else:
         outputxml = "%s/%s.xml" % (metadata_dir, outputfilename)
         fmd5      = "%s/%s.framemd5" % (provenance_dir, os.path.basename(filename))
         fmd5ffv1  = output + ".framemd5"
-
+        log       = "%s/%s_log.log" %  (log_dir,filename)
+        generate_log(log, 'Input = %s' % filename)
+        generate_log(log, 'Output = %s' % output) 
+        generate_log(log, 'makeffv1.py transcode to FFV1 and framemd5 generation of source started.') 
         # Transcode video file writing frame md5 and output appropriately
         subprocess.call(['ffmpeg',
                         '-i', filename,
@@ -143,17 +153,19 @@ else:
                         output,	
                         '-f','framemd5','-an'  # Create decoded md5 checksums for every frame of the input. -an ignores audio
                         , fmd5  ])
-        
-        
+        generate_log(log, 'makeffv1.py transcode to FFV1 and framemd5 generation completed.')        
+        generate_log(log, 'makeffv1.py Framemd5 generation of output file started.')
         subprocess.call(['ffmpeg',    # Create decoded md5 checksums for every frame of the ffv1 output
                         '-i',output,
                         '-report',
                         '-f','framemd5','-an',
                         fmd5ffv1 ])
+        generate_log(log, 'makeffv1.py Framemd5 generation of output file completed')                
         log_files =  glob('*.log')                
         for i in log_files:
             if 'ffmpeg' in i:
                 shutil.move(i, '%s/%s' % (log_dir,i))
+                generate_log(log, 'makeffv1.py %s moved to log directory' % i)
         # Verify that the video really is lossless by comparing the fixity of the two framemd5 files. 
         
         # Adapted from Andrew Dalke - http://stackoverflow.com/a/8304087/2188572
@@ -173,14 +185,17 @@ else:
                             checksum_mismatches.append(1)
         if len(checksum_mismatches) == 0:
             print 'LOSSLESS'
-            append_csv(csv_report_filename, (output,'LOSSLESS')) 
+            append_csv(csv_report_filename, (output,'LOSSLESS'))
+            generate_log(log, 'makeffv1.py Transcode was lossless') 
         elif len(checksum_mismatches) == 1:
             if checksum_mismatches[0] == 'sar':
                 print 'Image content is lossless, Pixel Aspect Ratio has been altered'
                 append_csv(csv_report_filename, (output,'LOSSLESS - different PAR'))
+                generate_log(log, 'makeffv1.py Image content is lossless, but Pixel Aspect Ratio has been altered')
         elif len(checksum_mismatches) > 1:
             print 'NOT LOSSLESS'     
             append_csv(csv_report_filename, (output,'NOT LOSSLESS'))
+            generate_log(log, 'makeffv1.py Not Lossless.')
 
         if filecmp.cmp(fmd5, fmd5ffv1, shallow=False): 
             print "YOUR FILES ARE LOSSLESS YOU SHOULD BE SO HAPPY!!!"
@@ -195,5 +210,8 @@ else:
         source_parent_dir = os.path.dirname(filename)
         relative_path = normpath.split(os.sep)[-1]
         manifest =  '%s_manifest.md5' % (relative_path)
+        generate_log(log, 'makeffv1.py MD5 manifest started')
         make_manifest(filenoext,manifest)
         os.chdir('..')
+        generate_log(log, 'makeffv1.py MD5 manifest completed')
+        
