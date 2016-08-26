1373ab81feb8bf2f1a6da2bd2219a02e0960115f
kieranjol <kieran.o.leary@gmail.com>
2016-08-26 14:23:38 +0100

    makedpx - add batch functionality

--- makedpx.py
+++ makedpx.py
@@ -9,7 +9,10 @@ import os
 from glob import glob
 from ififuncs import diff_textfiles
 from ififuncs import make_manifest
+import datetime
 
+
+print datetime.datetime.now()
 def remove_bad_files(root_dir):
     rm_these = ['.DS_Store', 'Thumbs.db', 'desktop.ini']
     for root, dirs, files in os.walk(root_dir):
@@ -21,62 +24,70 @@ def remove_bad_files(root_dir):
                     os.remove(path)
 
 source_directory = sys.argv[1]
-remove_bad_files(source_directory)
-source_parent_dir    = os.path.dirname(source_directory)
-normpath             = os.path.normpath(source_directory) 
-dirname              = os.path.split(os.path.basename(source_directory))[1]
-relative_path        = normpath.split(os.sep)[-1]
 
-
-source_manifest = source_parent_dir + '/%s_manifest.md5' % relative_path
-
-make_manifest(os.path.dirname(source_directory), os.path.basename(source_directory), source_manifest)
-def make_framemd5(directory, container):
-    os.chdir(directory)
-    images = glob('*.%s' % container)
-    global dirname
-    #dirname = ''
-    print images
-    numberless_filename = images[0].split("_")[0:-1]
-    ffmpeg_friendly_name = ''
-    counter = 0
-    while  counter <len(numberless_filename) :
-        ffmpeg_friendly_name += numberless_filename[counter] + '_'
-        counter += 1
-    output_dirname = dirname + '/' + ffmpeg_friendly_name + 'dpx_transcodes'
-    try:
-        os.makedirs(output_dirname + '/audio')
-        os.makedirs(output_dirname + '/image')
-        os.makedirs(output_dirname + '/image/logs')
-        os.makedirs(output_dirname + '/image/md5')
-        os.makedirs(output_dirname + '/image/dpx_files')
-        os.makedirs(output_dirname + '/image/xml_files')
-             
-    except: OSError
-    
-    output = output_dirname + '/image/md5/%s%s.framemd5' % (ffmpeg_friendly_name,container)
-    print output 
-    ffmpeg_friendly_name += "%06d." + '%s' % container
-    framemd5 = ['ffmpeg','-f','image2', '-i', ffmpeg_friendly_name,'-f','framemd5',output]
-    print framemd5
-    subprocess.call(framemd5)   
-    info = [output_dirname, output]
-    return info
+for root,dirnames,filenames in os.walk(source_directory):
+    if "tiff_scans"  in dirnames:
+        print root
+        source_directory = root + '/tiff_scans'
         
-info = make_framemd5(source_directory, 'tiff')
-output_dirname = info[0]  
-source_textfile = info[1]
-images = glob('*.tiff')
-for tiff in images:
-    cmd = ['ffmpegnometadata','-f','image2','-framerate','24', '-i', tiff ,output_dirname +  '/image/dpx_files' '/' + tiff[:-4] + 'dpx']
-    print cmd
-    subprocess.call(cmd)
-parent_basename =  os.path.basename(output_dirname)
-manifest_textfile = os.path.dirname(output_dirname) + '/' +  parent_basename + '_manifest.md5'
-other = make_framemd5(output_dirname + '/image/dpx_files', 'dpx')
-other_textfile = other[1]
-print source_textfile
-print other_textfile
-diff_textfiles(source_textfile, other_textfile)
-make_manifest(dirname, os.path.basename(output_dirname), manifest_textfile)
+       
+        
+        remove_bad_files(source_directory)
+        source_parent_dir    = os.path.dirname(source_directory)
+        normpath             = os.path.normpath(source_directory) 
+        dirname              = os.path.split(os.path.basename(source_directory))[1]
+        relative_path        = normpath.split(os.sep)[-1]
 
+
+        source_manifest = source_parent_dir + '/%s_manifest.md5' % relative_path
+
+        make_manifest(os.path.dirname(source_directory), os.path.basename(source_directory), source_manifest)
+        def make_framemd5(directory, container):
+            os.chdir(directory)
+            images = glob('*.%s' % container)
+            global dirname
+            #dirname = ''
+            print images
+            numberless_filename = images[0].split("_")[0:-1]
+            ffmpeg_friendly_name = ''
+            counter = 0
+            while  counter <len(numberless_filename) :
+                ffmpeg_friendly_name += numberless_filename[counter] + '_'
+                counter += 1
+            output_dirname = dirname + '/' + ffmpeg_friendly_name + 'dpx_transcodes'
+            try:
+                os.makedirs(output_dirname + '/image')
+                os.makedirs(output_dirname + '/image/logs')
+                os.makedirs(output_dirname + '/image/md5')
+                os.makedirs(output_dirname + '/image/dpx_files')
+                os.makedirs(output_dirname + '/image/xml_files')
+             
+            except: OSError
+    
+            output = output_dirname + '/image/md5/%s%s.framemd5' % (ffmpeg_friendly_name,container)
+            print output 
+            ffmpeg_friendly_name += "%06d." + '%s' % container
+            framemd5 = ['ffmpeg','-f','image2', '-i', ffmpeg_friendly_name,'-f','framemd5',output]
+            print framemd5
+            subprocess.call(framemd5)   
+            info = [output_dirname, output]
+            return info
+        
+        info = make_framemd5(source_directory, 'tiff')
+        output_dirname = info[0]  
+        source_textfile = info[1]
+        images = glob('*.tiff')
+        for tiff in images:
+            cmd = ['ffmpegnometadata','-f','image2','-framerate','24', '-i', tiff ,output_dirname +  '/image/dpx_files' '/' + tiff[:-4] + 'dpx']
+            print cmd
+            subprocess.call(cmd)
+        parent_basename =  os.path.basename(output_dirname)
+        manifest_textfile = os.path.dirname(output_dirname) + '/' +  parent_basename + '_manifest.md5'
+        other = make_framemd5(output_dirname + '/image/dpx_files', 'dpx')
+        other_textfile = other[1]
+        print source_textfile
+        print other_textfile
+        diff_textfiles(source_textfile, other_textfile)
+        make_manifest(dirname, os.path.basename(output_dirname), manifest_textfile)
+        print time
+        print datetime.datetime.now()
