3d362b28609897d6729930940cc014819a34c702
kieranjol <kieran.o.leary@gmail.com>
2016-09-28 18:05:54 +0100

    premis - overhaul:everything in functions, revtmd removed, cleanup

--- premis.py
+++ premis.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python
-'''
-Work in progress. I think this will just be a bunch of functions that other scripts can call on in order to use as a growing log file.
-Representation:
-1.1
-1.2
-1.4
-1.6 - original name
-1.7 - storage
-1.13 - Relationship - each image should relate to the representation.
-'''
-#http://stackoverflow.com/questions/7703018/how-to-write-namespaced-element-attributes-with-lxml
 import lxml.etree as ET
 import lxml.builder as builder
 import uuid
@@ -23,28 +11,7 @@ import hashlib
 from collections import OrderedDict
 import csv
 
-
-source_file = sys.argv[1]
-global items
-global revtmd
-items = pg.main()
-print items,'whatver'
-global manifest
-global csv_file
-manifest = os.path.dirname(os.path.abspath(source_file)) + '/' + os.path.basename(source_file) + '_manifest.md5'
-csv_file = os.path.expanduser("~/Desktop/premis_agents.csv")
-if os.path.isfile(csv_file):
-                    read_object = open(csv_file)
-                    reader = csv.reader(read_object)
-                    csv_list = list(reader)
-                    read_object.close()
-
-
-premisxml = os.path.dirname(os.path.dirname(sys.argv[1])) + '/' + os.path.basename(os.path.dirname(os.path.dirname(sys.argv[1]))) + '_premis.xml'
-
-def hashlib_md5(filename, manifest):
-   print filename
-   print manifest
+def hashlib_md5(source_file,filename, manifest):
    m = hashlib.md5()
    with open(str(filename), 'rb') as f:
        while True:
@@ -53,18 +20,17 @@ def hashlib_md5(filename, manifest):
                break
            m.update(buf)
    md5_output = m.hexdigest()
-   messageDigestAlgorithm.text = 'md5'
-   messageDigest.text = md5_output
    with open(manifest, "ab") as fo:
        fo.write(md5_output + '  ' + source_file.split(os.sep)[-1] + '/' + filename +  '\n')
+   return md5_output
 
     
 def add_value(value, element):
     element.text = value
     
-def write_premis(premisxml):
-    outFile = open(premisxml,'w')
-    doc.write(outFile,pretty_print=True)
+def write_premis(doc, premisxml):
+    with open(premisxml,'w') as outFile:
+        doc.write(outFile,pretty_print=True)
 
 def create_unit(index,parent, unitname):
     premis_namespace = "http://www.loc.gov/premis/v3"
@@ -78,7 +44,7 @@ def create_hash(filename):
     return md5    
 
 def get_input(filename):
-    global video_files
+
     # Input, either file or firectory, that we want to process.
     input = filename
     # Store the directory containing the input file/directory.
@@ -113,9 +79,10 @@ def get_input(filename):
     return video_files
     
 def make_premis(source_file):
-    main(source_file)   
-
-def make_agent(agentIdType_value,agentIdValue_value,agentName_value,agentVersion_value, agentType_value, agentNote_value, linkingEventIdentifier_value ):
+    xml_info = write_objects(source_file)   
+    return xml_info
+def make_agent(premis, agentIdType_value,agentIdValue_value,agentName_value,agentVersion_value, agentType_value, agentNote_value, linkingEventIdentifier_value ):
+    premis_namespace = "http://www.loc.gov/premis/v3"
     agent = ET.SubElement(premis, "{%s}agent" % (premis_namespace))
     premis.insert(-1, agent)
     agentIdentifier = create_unit(1,agent,'agentIdentifier')
@@ -135,47 +102,16 @@ def make_agent(agentIdType_value,agentIdValue_value,agentName_value,agentVersion
     linkingEventIdentifier.text = linkingEventIdentifier_value
     agent_info = [agentIdType_value,agentIdValue_value]
     return agent_info
-''''    
-def make_environment(agentIdType_value,agentIdValue_value,agentName_value,agentVersion_value, agentType_value, agentNote_value, linkingEventIdentifier_value ):
-    object_parent = create_unit(0, premis, 'object')
-    object_identifier_parent = create_unit(1,object_parent, 'objectIdentifier')
-    object_identifier_uuid_type = create_unit(1,object_identifier_uuid, 'objectIdentifierType')
-    object_identifier_uuid_type.text = 'locally defined identifier'
-    object_identifier_uuid_value = create_unit(2,object_identifier_uuid, 'objectIdentifierValue') 
-    object_identifier_uuid_value.text = 'Environment 1'
-    objectCategory = create_unit(2, object_parent, 'objectCategory')
-    objectCategory.text = 'intellectual entity'
-    environmentFunction = create_unit(2,object_parent,'environmentFunction')
-    environmentFunctionType = create_unit(1,environmentFunction,'environmentFunctionType')
-    environmentFunctionLevel = create_unit(2,environmentFunction,'environmentFunctionLevel')
-    environmentDesignation = create_unit(3,object_parent,'environmentDesignation')
-    environmentName = create_unit(1,environmentDesignation,'environmentName')
-    environmentVersion = create_unit(2,environmentDesignation,'environmentVersion')
-    environmentOrigin = create_unit(3,environmentDesignation,'environmentOrigin')
-    environmentDesignationNote = create_unit(4,environmentDesignation,'DesignationNote')
-    environmentDesignationExtension = create_unit(5,environmentDesignation,'environmentDesignationExtension')
-    relationship = create_unit(4, object_parent, 'relationship')
-    relationshipType = create_unit(0,relationship, 'relationshipType')
-    relationshipType.text = 'structural'
-    relationshipSubType = create_unit(0,relationship, 'relationshipSubType')
-    relationshipSubType.text = '???'
     
-'''    
-def make_event(event_type, event_detail, agent1, agent2, eventID ):
+def make_event(premis,event_type, event_detail, agent1, agent2, eventID ):
         print agent1, agent2
         agent1type =agent1[0]
         agent1value =agent1[1]
         agent2type =agent2[0]
         agent2value =agent2[1]
-        global linkingObjectIdentifierValue
-        global event_Type
-        
-        global revtmd_capture_history
-        
+        premis_namespace = "http://www.loc.gov/premis/v3"
         event = ET.SubElement(premis, "{%s}event" % (premis_namespace))
         premis.insert(-1,event)
-        #event_Identifier = ET.Element("{%s}eventIdentifier" % (premis_namespace))
-        #event.insert(1,event_Identifier)
         event_Identifier = create_unit(1,event,'event_Identifier')
         event_id_type = ET.Element("{%s}eventIdentifierType" % (premis_namespace))
         event_Identifier.insert(0,event_id_type)
@@ -214,76 +150,6 @@ def make_event(event_type, event_detail, agent1, agent2, eventID ):
         linkingAgentIdentifierType2.text = agent2type 
         linkingAgentIdentifierValue2.text = agent2value
 
-
-        
-        '''''
-        if event_type == 'capture':
-            
-            eventDetailExtension = create_unit(1, eventDetailInformation, 'eventDetailExtension')
-            revtmd_first = create_revtmd_unit(0, eventDetailExtension, 'object')
-            revtmd_filename = create_revtmd_unit(1, revtmd_first, 'filename')
-            revtmd_organisation = create_revtmd_unit(2, revtmd_first, 'organisation')
-            revtmd_organisation_main = create_revtmd_unit(0, revtmd_organisation, 'organisation_main')
-            revtmd_organisation_main_name = create_revtmd_unit(0, revtmd_organisation_main, 'name')
-            revtmd_organisation_main_role = create_revtmd_unit(0, revtmd_organisation_main, 'role')
-    
-            revtmd_organisation_division = create_revtmd_unit(1, revtmd_organisation, 'organisation_divsion')
-            revtmd_organisation_division_name = create_revtmd_unit(0, revtmd_organisation_division, 'name')
-            revtmd_use = create_revtmd_unit(3, revtmd_first, 'use')
-            revtmd_use.text = 'Preservation Master'
-            revtmd_capture_history = create_revtmd_unit(4, revtmd_first, 'captureHistory')
-            revtmd_digitizationDate = create_revtmd_unit(1, revtmd_capture_history, 'digitizationDate')
-            revtmd_digitizationEngineer = create_revtmd_unit(2, revtmd_capture_history, 'digitizationEngineer')
-            if items["workflow"] == 'scanning':
-                for i in items['prepList']:
-                    counter = 3
-                    x = create_revtmd_unit(counter, revtmd_capture_history, 'preparationActions')
-                    x.text = i
-                    counter += 1
-                process_history(revtd_basement_2k_scanner, 1)
-                process_history(revtd_basement_2k_scanner_host_computer, 2) 
-                process_history(revtd_basement_2k_scanner_host_computer_os, 3)
-                process_history(revtd_basement_2k_scanner_software, 4) 
-                process_history(revtmd_basement_content_agent_pc, 5) 
-                process_history(revtmd_basement_content_agent_os, 6) 
-                
-            if items["workflow"] == 'tape':
-                revtmd_edit_2_computer = (('role','Host Computer'), ('manufacturer','Apple'), ('modelName','Mac Pro'), ('serialNumber','???'))
-                revtmd_edit_2_computer_os = (('role','Host Computer Operating System'), ('manufacturer','Apple'), ('version','El Capitan'))
-                revtmd_edit_2_blackmagic_hardware = (('role','Capture Card'), ('manufacturer','Blackmagic'), ('modelName','Ultrastudio 4k'),('signal','SDI'), ('serialNumber','2965997'))
-                revtmd_edit_2_ffmpeg = (('role','Scanner'), ('manufacturer','P&S Techniks'), ('modelName','Steadyframe'),('signal','Ethernet'), ('serialNumber','601-0101'))
-                revtmd_edit_2_monitor = (('role','Host Computer Monitor'), ('manufacturer','LG'), ('modelName','???'),('signal','HDMI'), ('serialNumber','???'))
-                revtmd_edit_2_speakers = (('role','Scanner'), ('manufacturer','P&S Techniks'), ('modelName','Steadyframe'),('signal','Ethernet'), ('serialNumber','601-0101'))
-                revtmd_edit_2_headphones = (('role','Scanner'), ('manufacturer','P&S Techniks'), ('modelName','Steadyframe'),('signal','Ethernet'), ('serialNumber','601-0101'))
-                revtmd_edit_2_blackmagic_capture_software = (('role','Capture Software'), ('manufacturer','Blackmagic'), ('modelName','Media Express'),('signal','SDI'), ('serialNumber','???'))
-                
-                
-                process_history(OrderedDict(revtmd_edit_2_computer), 1)
-                process_history(OrderedDict(revtmd_edit_2_computer_os), 2)
-                process_history(OrderedDict(revtmd_edit_2_blackmagic_hardware), 3)
-                process_history(OrderedDict(revtmd_edit_2_monitor), 4)
-                process_history(OrderedDict(revtmd_edit_2_blackmagic_capture_software), 5)
-        '''
-        '''
-                parser = ET.XMLParser(remove_blank_text=True)
-                blaa = ET.fromstring(*args, parser=parser)
-            
-                eventDetailExtension.insert(0,blaa)  
-        '''
-        return doc
-global vtr        
-vtr = {'role':'playback', 'manufacturer':'Sony', 'signal':'composite', 'serialNumber':'abc123'}
-
-vtr2 = {'role':'playback', 'manufacturer':'Panasonic', 'signal':'composite', 'serialNumber':'abc123'}
-revtmd_basement_content_agent_pc = {'role':'Host Computer', 'manufacturer':'Hewlett-Packard Company', 'modelName':'Z800 Workstation', 'serialNumber':'CZC2082R1Z'}
-revtmd_basement_content_agent_os = {'role':'Host Computer Operating System', 'manufacturer':'Microsoft', 'version':'Windows 7 Professional'}
-revtd_basement_2k_scanner = (('role','Scanner'), ('manufacturer','P&S Techniks'), ('modelName','Steadyframe'),('signal','Ethernet'), ('serialNumber','601-0101'))
-revtd_basement_2k_scanner = OrderedDict(revtd_basement_2k_scanner)
-revtd_basement_2k_scanner_host_computer = {'role':'Scanner Host Computer', 'manufacturer':'???', 'modelName':'???', 'serialNumber':'???'}
-revtd_basement_2k_scanner_host_computer_os = {'role':'Scanner Host Computer Operating System', 'manufacturer':'Debian', 'modelName':'???', 'serialNumber':'???'}
-revtd_basement_2k_scanner_software = {'role':'Captures Image Sequence', 'manufacturer':'P&S Techniks', 'modelName':'???', 'serialNumber':'???'}
-
-
         
 def process_history(coding_dict, process_history_placement):
     
@@ -296,13 +162,26 @@ def process_history(coding_dict, process_history_placement):
         a.text = coding_dict[i]
         counter1 += 1
         
+def main():
+        source_file = sys.argv[1]
+        xml_info    = make_premis(source_file)
+        print xml_info
         
-def main(source_file):
+        doc         = xml_info[0]
+        premisxml   = xml_info[1]
+        print premisxml
+        write_premis(doc, premisxml)    
+def write_objects(source_file):
+    '''''
     global premis_namespace
     global premis
     global messageDigestAlgorithm
     global messageDigest
-    global doc    
+    global doc   
+    ''' 
+    manifest = os.path.dirname(os.path.abspath(source_file)) + '/' + os.path.basename(source_file) + '_manifest.md5'
+    items = pg.main()
+    premisxml = os.path.dirname(os.path.dirname(sys.argv[1])) + '/' + os.path.basename(os.path.dirname(os.path.dirname(sys.argv[1]))) + '_premis.xml'
     namespace = '<premis:premis xmlns:premis="http://www.loc.gov/premis/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:revtmd="http://nwtssite.nwts.nara/schema/" xsi:schemaLocation="http://www.loc.gov/premis/v3 https://www.loc.gov/standards/premis/premis.xsd http://nwtssite.nwts.nara/schema/  " version="3.0"></premis:premis>'
     premis = ET.fromstring(namespace)
     premis_namespace = "http://www.loc.gov/premis/v3"
@@ -310,7 +189,6 @@ def main(source_file):
     doc = ET.ElementTree(premis)
     video_files = get_input(source_file)
     mediainfo_counter = 1
-    #new_element = ET.Element('premis:object', namespaces={'ns': 'premis'})
     # Assuming that directory input means image sequence...
     
 
@@ -327,6 +205,7 @@ def main(source_file):
         filetype = 'image'
         print video_files
         object_parent = create_unit(0, premis, 'object')
+        print 'first_object'
         object_identifier_parent = create_unit(1,object_parent, 'objectIdentifier')
         object_identifier_uuid = create_unit(0,object_parent, 'objectIdentifier')
         object_identifier_uuid_type = create_unit(1,object_identifier_uuid, 'objectIdentifierType')
@@ -361,7 +240,8 @@ def main(source_file):
         representationrelatedObjectIdentifierValue.text = root_uuid
     rep_counter = 0
     for image in video_files:
-        object_parent = create_unit(-1,premis, 'object')
+        print 'other'
+        object_parent = create_unit(mediainfo_counter,premis, 'object')
         object_identifier_parent = create_unit(1,object_parent, 'objectIdentifier')
         ob_id_type = ET.Element("{%s}objectIdentifierType" % (premis_namespace))
         ob_id_type.text = 'IFI Irish Film Archive Object Entry Number'
@@ -425,12 +305,19 @@ def main(source_file):
             relationshipType.text = 'structural'
             relationshipSubType = create_unit(1,relationship, 'relationshipSubType')
             relationshipSubType.text = 'is included in'
-        #create_hash(image)
-        hashlib_md5(image, manifest)
+        messageDigestAlgorithm.text = 'md5'
+        md5_output = hashlib_md5(source_file, image, manifest)
+        messageDigest.text = md5_output
+        
         mediainfo_counter += 1
 
-    #make_event('Compression')
-    
+
+    csv_file = os.path.expanduser("~/Desktop/premis_agents.csv")
+    if os.path.isfile(csv_file):
+                        read_object = open(csv_file)
+                        reader = csv.reader(read_object)
+                        csv_list = list(reader)
+                        read_object.close()
     if items["workflow"] == 'scanning':
         for lists in csv_list:
             for item in lists:
@@ -441,19 +328,12 @@ def main(source_file):
                     scanner_info = lists
                     
         capture_uuid = str(uuid.uuid4())
-        scannerAgent  = make_agent(scanner_info[0],scanner_info[1], scanner_info[2], scanner_info[3], scanner_info[4],scanner_info[5],capture_uuid )
-        operatorAgent = make_agent(user_info[0],user_info[1], user_info[2],'', user_info[3], '', capture_uuid )
-        #make_event('Message Digest Calculation', 'Program="md5deep" Version="4.4"', scannerAgent,operatorAgent)
-        make_event('capture', '', scannerAgent, operatorAgent, capture_uuid)
-
-    '''
-    >>> parser = etree.XMLParser(remove_blank_text=True)
-    >>> tree = etree.parse(filename, parser)
-    '''
-
-    #print(ET.tostring(doc, pretty_print=True))
-    
+        scannerAgent  = make_agent(premis,scanner_info[0],scanner_info[1], scanner_info[2], scanner_info[3], scanner_info[4],scanner_info[5],capture_uuid )
+        operatorAgent = make_agent(premis,user_info[0],user_info[1], user_info[2],'', user_info[3], '', capture_uuid )
+        make_event(premis, 'capture', '', scannerAgent, operatorAgent, capture_uuid)
+    xml_info = [doc, premisxml]
+    return xml_info
     
 if __name__ == "__main__":
-        make_premis(source_file)
-        write_premis(premisxml)
+        main()
+
