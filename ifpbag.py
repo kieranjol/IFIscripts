0b69ee85d4279e68ea41b483d47b3ecff2b32201
kieranjol <kieran.o.leary@gmail.com>
2016-10-12 21:59:32 +0100

    remove unused scripts

--- ifpbag.py
+++
@@ -1,7 +0,0 @@
-import sys
-import bagit
-
-bag_dir = sys.argv[1]
-
-bag = bagit.make_bag(bag_dir, {'Contact-Name': 'Dean Kavanagh','Source-Organization': 'Irish Film Institute','Contact-Email': 'dkavanagh@irishfilm.ie'})
-print bag.entries
