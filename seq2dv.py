16cc845110a459f1be040b95b1c72e2c99d13209
kieranjol <kieran.o.leary@gmail.com>
2016-10-21 09:58:23 +0100

    seq2dv - initial commit

---
+++ seq2dv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+import os
+from glob import glob
+
+source_directory = sys.argv[1]
+os.chdir(source_directory)
+images = (
+        glob('*.tif') +
+        glob('*.tiff') +
+        glob('*.dpx') 
+        )
+
+numberless_filename = images[0].split("_")[0:-1]
+ffmpeg_friendly_name = ''
+counter = 0
+while  counter <len(numberless_filename) :
+    ffmpeg_friendly_name += numberless_filename[counter] + '_'
+    counter += 1
+dirname = os.path.dirname(source_directory)    
+output = dirname + '/%s_consolidate.mov' % os.path.split(source_directory)[-1]
+ffmpeg_friendly_name += '%06d.tiff'
+cmd = ['ffmpeg','-f','image2','-framerate','24', '-i', ffmpeg_friendly_name,'-c:v','dvvideo','-vf','scale=720x576',output]
+print cmd
+subprocess.call(cmd)
