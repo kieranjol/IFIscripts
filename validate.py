7bc2c24a3afbeb5cc1aefbf22581302749e25979
kieranjol <kieran.o.leary@gmail.com>
2016-10-22 16:07:49 +0100

    validate - adds check for files missing from manifest

--- validate.py
+++ validate.py
@@ -29,18 +29,31 @@ def get_input(manifest):
     
 
 def parse_manifest(manifest):
+    missing_files = 0
     manifest_dict = {}
+    os.chdir(os.path.dirname(manifest))
     with open(manifest, 'rb') as manifest_object:
         manifest_list = manifest_object.readlines()
         for entries in manifest_list:
             checksum = entries.split(' ')[0]
             path = entries[34:].rstrip()
-            logging.debug('%s %s' % (checksum, path))
-            manifest_dict[path] = checksum
-        logging.debug(manifest_dict)
-    return manifest_dict 
+            if not os.path.isfile(path):
+                logging.info('%s is missing' % path )
+                print '%s is missing' % path
+                missing_files += 1
+            elif os.path.isfile(path):
+                logging.debug('%s %s' % (checksum, path))
+                manifest_dict[path] = checksum
+                logging.debug(manifest_dict)
+    if missing_files > 0:
+        print 'The number of missing files: %s' % missing_files
+        logging.info('The number of missing files is: %s' %  missing_files)
+    elif missing_files == 0:
+        print 'All files present'
+        logging.info('All files present')
+    return manifest_dict, missing_files
 
-def validate(manifest_dict, manifest):
+def validate(manifest_dict, manifest,missing_files):
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
     logging.info('Validating %s ' % manifest)
     error_counter = 0
@@ -48,7 +61,6 @@ def validate(manifest_dict, manifest):
     manifest_directory = os.path.dirname(manifest)
     os.chdir(manifest_directory)
     for i in manifest_dict:
-        logging.debug(i)
         print 'Validating %s' % i
         current_hash = hashlib_md5(i)
         if current_hash == manifest_dict[i]:
@@ -61,8 +73,12 @@ def validate(manifest_dict, manifest):
         print 'The number of mismatched checksums is: %s' % error_counter
         logging.info('The number of mismatched checksums is: %s' %  error_counter)
     elif error_counter == 0:
-        print 'All checksums have validated'
-        logging.info('All checksums have validated')
+        if missing_files > 0:
+            print 'The number of missing files: %s' % missing_files
+            logging.info('The number of mismatched checksums is: %s' %  missing_files)
+        elif missing_files == 0:
+            print 'All checksums have validated'
+            logging.info('All checksums have validated')
 
 def make_parser():
     parser = argparse.ArgumentParser(description='MD5 checksum manifest validator. Currently this script expects an md5 checksum, followed by two spaces, followed by a file path.'
@@ -72,8 +88,8 @@ def make_parser():
 
 def check_manifest(input):
     manifest = get_input(input)
-    manifest_dict = parse_manifest(manifest)
-    validate(manifest_dict, manifest)
+    manifest_dict, missing_files = parse_manifest(manifest)
+    validate(manifest_dict, manifest, missing_files)
 def main():
     parser = make_parser()
     args = parser.parse_args()
