b4642960679867aa7041eabb011e4f5d43c94977
kieranjol <kieran.o.leary@gmail.com>
2016-11-15 16:13:59 +0000

    update - adds warning about new log/manifest desktop directories!

--- update.py
+++ update.py
@@ -8,3 +8,5 @@ os.chdir(home)
 os.chdir('ifigit/ifiscripts')
 print os.getcwd()
 subprocess.call(['git', 'pull'])
+print '\nNotice - 2016-11-15'
+print '********** Logs and manifests for moveit.py and validate.py are now in subfolders on the desktop!!!***************'
