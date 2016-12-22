069252bfb4bee1132ae909d2cbcaabdbdcf1ec85
kieranjol <kieran.o.leary@gmail.com>
2016-12-22 17:42:19 +0000

    validate.py - adds @ecodonohoe progress meter

--- validate.py
+++ validate.py
@@ -11,13 +11,24 @@ from ififuncs import make_desktop_logs_dir
 
 
 def hashlib_md5(filename):
+   read_size = 0
+   last_percent_done = 0
    m = hashlib.md5()
+   total_size = os.path.getsize(filename)
    with open(str(filename), 'rb') as f:
        while True:
            buf = f.read(2**20)
            if not buf:
                break
+           read_size += len(buf)
            m.update(buf)
+           percent_done = 100 * read_size / total_size
+           if percent_done > last_percent_done:
+               sys.stdout.write('[%d%%]\r' % percent_done)
+               sys.stdout.flush()
+
+
+               last_percent_done = percent_done
    md5_output = m.hexdigest()
    return md5_output
    
