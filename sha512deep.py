6341e39e9f08b8a3800a7d4c3b7e31fdd11974a6
kieranjol <kieran.o.leary@gmail.com>
2016-12-24 00:22:36 +0000

    sha512deep - cleans up variables and print manifest location

--- sha512deep.py
+++ sha512deep.py
@@ -3,20 +3,24 @@
 import os
 import subprocess
 import sys
-source = sys.argv[1]
 
-source_count = 0
-for root, directories, filenames in os.walk(source):   
-    for files in filenames:   
-            source_count +=1 
-counter2 = 1
-manifest = os.path.dirname(source) + '/manifest_sha512.txt'
-for root, directories, filenames in os.walk(source):   
-    for files in filenames:   
-            print 'processing %s - %d of %d' % (files, counter2, source_count)
-            sha512 = subprocess.check_output(['openssl', 'sha512', '-r', os.path.join(root, files)])
-            root2 = root.replace(os.path.dirname(source), '')
-            
-            with open(manifest, "ab") as fo:
-                fo.write(sha512[:128] + ' ' + os.path.join(root2,files ).replace("\\", "/") + '\n')
-            counter2 += 1
+def main():
+    source = sys.argv[1]
+    total_file_count = 0
+    print 'Determining number of files to be processed'
+    for root, directories, filenames in os.walk(source):
+        for files in filenames:
+            total_file_count +=1
+    progress_counter = 1
+    manifest = os.path.dirname(source) + '/manifest_sha512.txt'
+    for root, directories, filenames in os.walk(source):
+        for files in filenames:
+                print 'processing %s - %d of %d' % (files, progress_counter, total_file_count)
+                sha512 = subprocess.check_output(['openssl', 'sha512', '-r', os.path.join(root, files)])
+                root2 = root.replace(os.path.dirname(source), '')
+                with open(manifest, "ab") as fo:
+                    fo.write(sha512[:128] + ' ' + os.path.join(root2,files ).replace("\\", "/") + '\n')
+                progress_counter += 1
+    print 'Manifest created in %s' % manifest
+if __name__ == '__main__':
+    main()
