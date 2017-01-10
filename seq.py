a8612998c7e8e1f118ae390e9df3fb53120136b1
kieranjol <kieran.o.leary@gmail.com>
2017-01-10 14:42:00 +0000

    seq - adds argparse and prores option

--- seq.py
+++ seq.py
@@ -3,17 +3,30 @@
 import subprocess
 import sys
 import os
+import argparse
 from glob import glob
 
-source_directory = sys.argv[1]
+
+
+parser = argparse.ArgumentParser(description='Generate v210/mov file from image sequence.'
+                                'Written by Kieran O\'Leary.')
+parser.add_argument('input', help='file path of parent directory')
+parser.add_argument('-p', action='store_true', help='Use the Apple ProRes 4:2:2 codec instead of v210')
+args = parser.parse_args()
+source_directory = args.input
+
+if not os.path.isdir(args.input):
+    print('Please provide a directory as input, not a file')
+    sys.exit()
 os.chdir(source_directory)
 images = (
         glob('*.tif') +
         glob('*.tiff') +
         glob('*.dpx') 
         )
-
+extension = os.path.splitext(images[0])[1]
 numberless_filename = images[0].split("_")[0:-1]
+
 ffmpeg_friendly_name = ''
 counter = 0
 while  counter <len(numberless_filename) :
@@ -21,7 +34,11 @@ while  counter <len(numberless_filename) :
     counter += 1
 dirname = os.path.dirname(source_directory)    
 output = dirname + '/%s_consolidate.mov' % os.path.split(source_directory)[-1]
-ffmpeg_friendly_name += '%06d.tiff'
-cmd = ['ffmpeg','-f','image2','-framerate','24', '-i', ffmpeg_friendly_name,'-c:v','v210',output]
+ffmpeg_friendly_name += '%06d' + extension
+codec = 'v210'
+if args.p:
+    codec = 'prores'
+cmd = ['ffmpeg','-f','image2','-framerate','24', '-i', ffmpeg_friendly_name,'-c:v',codec,output]
 print cmd
 subprocess.call(cmd)
+print 'Output file is located in %s' % output
