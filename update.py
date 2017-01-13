3e8c29bb4e739bda22b902751a8541d55f2603bc
kieranjol <kieran.o.leary@gmail.com>
2017-01-13 10:48:18 +0000

    update.py - adds premisviewer updates and install check

--- update.py
+++ update.py
@@ -3,10 +3,28 @@ import os
 import subprocess
 import sys
 
-home = os.path.expanduser("~/")
-os.chdir(home)
-os.chdir('ifigit/ifiscripts')
-print os.getcwd()
-subprocess.call(['git', 'pull'])
-print '\nNotice - 2016-11-15'
-print '********** Logs and manifests for moveit.py and validate.py are now in subfolders on the desktop!!!***************'
+
+
+def install_check(directory):
+    if os.path.isdir(directory):
+        return True
+    else:
+        print('\nWARNING - %s does not exist!\nCreate a directory in your home directory called `ifigit` and git clone the repository in there') % directory
+        return False
+
+def main():
+    home = os.path.expanduser("~/")
+    os.chdir(home)
+    if install_check('ifigit/ifiscripts'):
+        os.chdir('ifigit/ifiscripts')
+        print('Updating IFIScripts - Changing directory to %s and running `git pull`') %os.getcwd()
+        subprocess.call(['git', 'pull'])
+    if install_check('../premisviewer'):
+        os.chdir('../premisviewer')
+        print('Updating PREMISviewer - Changing directory to %s and running `git pull`') %os.getcwd()
+        subprocess.call(['git', 'pull'])
+    print('\nNotice - 2017-01-13')
+    print ('********** PREMISVIEWER is now part of update.py \n***************')
+
+if __name__ == '__main__':
+    main()
