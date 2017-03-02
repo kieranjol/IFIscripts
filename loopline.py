2781fd199eb76d0ed4d161a8b0f9489967530cfb
kieranjol <kieran.o.leary@gmail.com>
2017-03-02 00:10:06 +0000

    loopline - allows bitc to run on windows O.S

--- loopline.py
+++ loopline.py
@@ -7,12 +7,16 @@ import makeffv1
 def main():
     video_files, csv_report_filename = makeffv1.get_input()
     makeffv1.make_ffv1(video_files, csv_report_filename)
+    if os.path.isdir(sys.argv[1]):
+       output = sys.argv[1]
+    elif os.path.isfile(sys.argv[1]):
+       output = os.path.dirname(sys.argv[1])
     for video in video_files:
-        subprocess.call(['bitc.py',
+        subprocess.check_call([sys.executable,os.path.expanduser("~/ifigit/ifiscripts/bitc.py"),
         '-clean','-yadif',
         '-crf','20',
-        video,
-        '-o', os.path.dirname(sys.argv[1])])
+        os.path.join(output,video),
+        '-o', output])
 
 
 if __name__ == '__main__':
