5baca3b992c51d7b9b279c24e6a4fad48c4b28f4
kieranjol <kieran.o.leary@gmail.com>
2017-03-06 00:15:45 +0000

    tapepremis - adds placeholder tape premis workflow

---
+++ tapepremis.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import subprocess
+import argparse
+import sys
+import os
+import hashlib
+import shutil
+import uuid
+import time
+import uuid
+from glob import glob
+from ififuncs import hashlib_manifest
+from ififuncs import get_date_modified
+from premis import make_premis
+from premis import write_premis
+from premis import make_agent
+from premis import make_event
+from premis import setup_xml
+from premis import create_representation
+from premis import create_intellectual_entity
+
+
+def main():
+    premisxml, premis_namespace, doc, premis = setup_xml(sys.argv[1])
+    print premisxml, doc, premis
+    source_file = sys.argv[1]
+    items = {"workflow":"raw audio","oe":os.path.basename(source_file), "filmographic":'n/a', "sourceAccession":os.path.basename(source_file), "interventions":['placeholder'], "prepList":['placeholder'], "user":'Kieran O\' Leary'}
+    representation_uuid = str(uuid.uuid4())
+    xml_info = make_premis(source_file, items, premis, premis_namespace, premisxml,representation_uuid,'not_sequence')
+    capture_uuid                                = str(uuid.uuid4())
+    transcode_uuid                              = str(uuid.uuid4())
+    framemd5_uuid                               = str(uuid.uuid4())
+    manifest_uuid                               = str(uuid.uuid4())
+    ffmpegAgent                                 = make_agent(premis,[transcode_uuid] , 'ee83e19e-cdb1-4d83-91fb-7faf7eff738e')
+    make_event(premis, 'creation', 'transcode to ffv1 (figure out wording later)', [ffmpegAgent], transcode_uuid,xml_info[4], 'outcome', 'now-placeholder')
+    
+    write_premis(doc, premisxml)
+
+if __name__ == '__main__':
+    main()
