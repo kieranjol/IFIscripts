00a229bf893da81956838f164e0d35614fd9e95c
kieranjol <kieran.o.leary@gmail.com>
2017-03-07 23:30:53 +0000

    tapepremis - makes a bit more progress w/ interview

--- tapepremis.py
+++ tapepremis.py
@@ -21,7 +21,7 @@ from premis import create_representation
 from premis import create_intellectual_entity
 
 
-def capture_description(premis, xml_info):
+def capture_description(premis, xml_info,capture_station):
     '''
     Events:
     1. capture - glean from v210 mediainfo xml
@@ -35,9 +35,9 @@ def capture_description(premis, xml_info):
     framemd5_uuid                               = str(uuid.uuid4())
     manifest_uuid                               = str(uuid.uuid4())
     ffmpegAgent                                 = make_agent(premis,[transcode_uuid] , 'ee83e19e-cdb1-4d83-91fb-7faf7eff738e')
-    m200pAgent                                 = make_agent(premis,[transcode_uuid] , '60ae3a85-b595-45e0-8e4a-b95e90a6c422')
+    m2000pAgent                                  = make_agent(premis,[transcode_uuid] , '60ae3a85-b595-45e0-8e4a-b95e90a6c422')
     
-    capture_agents = [ffmpegAgent, m200pAgent]
+    capture_agents = [ffmpegAgent, m2000pAgent]
     make_event(premis, 'creation', 'transcode to ffv1 (figure out wording later)', capture_agents, transcode_uuid,xml_info[4], 'outcome', 'now-placeholder')
     
     
@@ -57,25 +57,33 @@ def get_capture_workstation(mediaxml):
     fcp7_check =  len(mxml.xpath('//COMAPPLEFINALCUTSTUDIOMEDIAUUID'))
     if mediaExpress_check > 0:
         print 'this was probably Media Express?'
+        capture_station = 'es2'
     elif fcp7_check > 0:
         print 'this was probably FCP7?'
+        capture_station = 'loopline'
     else:
         # i can't find any meaningful distinctive metadata that control room writes.
         print 'this was probably Control Room?'
+        capture_station = 'ingest1'
+    print 'Does this sound ok? Y/N?'
+    station_confirm = ''    
+    while station_confirm not in ('Y','y','N','n'):
+            station_confirm = raw_input()
+            if station_confirm not in ('Y','y','N','n'):
+                print 'Incorrect input. Please enter Y or N'
 
-    sys.exit()
-    capture_station = ''
-    if not capture_station == '1' or capture_station == '2' or capture_station == '3':
-        capture_station =  raw_input('\n\n**** Where was tape captured?\nPress 1, 2 or 3\n\n1. es2\n2. loopline\n3. ingest 1\n' )
-        while capture_station not in ('1','2','3'):
-            capture_station =  raw_input('\n\n**** Where was tape captured?\nPress 1, 2 or 3\n\n1. es2\n2. loopline\n3. ingest 1\n' )
-    if capture_station == '1':
-        capture_station = 'telecine'
-    elif capture_station == '2':
-        capture_station = 'ca_machine'
-    elif capture_station == '3':
-        capture_station = 'ca_machine'
-    return capture_station
+            capture_station = ''
+            if not capture_station == '1' or capture_station == '2' or capture_station == '3':
+                capture_station =  raw_input('\n\n**** Where was tape captured?\nPress 1, 2 or 3\n\n1. es2\n2. loopline\n3. ingest 1\n' )
+                while capture_station not in ('1','2','3'):
+                    capture_station =  raw_input('\n\n**** Where was tape captured?\nPress 1, 2 or 3\n\n1. es2\n2. loopline\n3. ingest 1\n' )
+            if capture_station == '1':
+                capture_station = 'es2'
+            elif capture_station == '2':
+                capture_station = 'loopline'
+            elif capture_station == '3':
+                capture_station = 'ingest1'
+            return capture_station
 
 
 def main():
@@ -87,7 +95,10 @@ def main():
     metadata_dir = os.path.join(parent_dir, 'metadata')
     ffv1_xml = os.path.join(metadata_dir, os.path.basename(sys.argv[1] + '_mediainfo.xml'))
     if os.path.isfile(ffv1_xml):
-        get_capture_workstation(ffv1_xml)
+        capture_sation = get_capture_workstation(ffv1_xml)
+    else:
+        print('Can\'t find XML of FFv1 file. Exiting!')
+        sys.exit() 
     '''
     /home/kieranjol/ifigit/ifiscripts/massive/objects sip
     /home/kieranjol/ifigit/ifiscripts/massive parent
@@ -102,7 +113,7 @@ def main():
     representation_uuid = str(uuid.uuid4())
     # the final argument here is 'loopline' which tells premis.py to not generate a checksum
     xml_info = make_premis(source_file, items, premis, premis_namespace, premisxml,representation_uuid,md5)
-    capture_description(premis, xml_info)
+    capture_description(premis, xml_info, capture_station)
     
     
     
