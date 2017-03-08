c679f484c3b5ac0f946bd295641778eb8a1a2dce
kieranjol <kieran.o.leary@gmail.com>
2017-03-08 09:33:47 +0000

    tapepremis/premis_agents - more events and agents

--- tapepremis.py
+++ tapepremis.py
@@ -35,12 +35,20 @@ def capture_description(premis, xml_info,capture_station):
     framemd5_uuid                               = str(uuid.uuid4())
     manifest_uuid                               = str(uuid.uuid4())
     ffmpegAgent                                 = make_agent(premis,[transcode_uuid] , 'ee83e19e-cdb1-4d83-91fb-7faf7eff738e')
-    m2000pAgent                                 = make_agent(premis,[transcode_uuid] , '60ae3a85-b595-45e0-8e4a-b95e90a6c422')
-    
-    capture_agents = [ffmpegAgent, m2000pAgent]
-    make_event(premis, 'creation', 'tape capture', capture_agents, transcode_uuid,xml_info[4], 'outcome', 'now-placeholder')
+    j30sdiAgent                                 = make_agent(premis,[capture_uuid] , 'e2ca7ad2-8edf-4e4e-a3c7-36e970c796c9')
+    bm4kAgent                                   = make_agent(premis,[capture_uuid] , 'f47b98a2-b879-4786-9f6b-11fc3234a91e')
+    edit_suite2_macAgent                        = make_agent(premis,[capture_uuid] , '75a0b9ff-1f04-43bd-aa87-c31b73b1b61c')
+    m2000pAgent                                 = make_agent(premis,[capture_uuid] , '60ae3a85-b595-45e0-8e4a-b95e90a6c422')
+    elcapitanAgent                              = make_agent(premis,[capture_uuid] , '68f56ede-a1cf-48aa-b1d8-dc9850d5bfcc')
+    print capture_station
+    if capture_station == 'es2':
+        capture_agents = [j30sdiAgent, bm4kAgent, edit_suite2_macAgent, elcapitanAgent]
+    if capture_station == 'loopline':
+        capture_agents = [j30sdiAgent, bm4kAgent, edit_suite2_macAgent, elcapitanAgent]
+    make_event(premis, 'creation', 'tape capture', capture_agents, capture_uuid,xml_info[4], 'outcome', 'now-placeholder')
     make_event(premis, 'compression', 'transcode to ffv1 (figure out wording later)', capture_agents, transcode_uuid,xml_info[4], 'outcome', 'now-placeholder')
-    make_event(premis, 'fixity check', 'lossless verification via framemd5 (figure out wording later)', capture_agents, transcode_uuid,xml_info[4], 'outcome', 'now-placeholder')
+    make_event(premis, 'fixity check', 'lossless verification via framemd5 (figure out wording later)', capture_agents, framemd5_uuid,xml_info[4], 'source', 'now-placeholder')
+    make_event(premis, 'message digest calculation', 'whole file checksum manifest of SIP', capture_agents, manifest_uuid,xml_info[4], 'source', 'now-placeholder')
     
     
 def get_checksum(manifest):
@@ -68,24 +76,25 @@ def get_capture_workstation(mediaxml):
         print 'this was probably Control Room?'
         capture_station = 'ingest1'
     print 'Does this sound ok? Y/N?'
+    print capture_station
     station_confirm = ''    
     while station_confirm not in ('Y','y','N','n'):
             station_confirm = raw_input()
             if station_confirm not in ('Y','y','N','n'):
                 print 'Incorrect input. Please enter Y or N'
-
-            capture_station = ''
-            if not capture_station == '1' or capture_station == '2' or capture_station == '3':
-                capture_station =  raw_input('\n\n**** Where was tape captured?\nPress 1, 2 or 3\n\n1. es2\n2. loopline\n3. ingest 1\n' )
-                while capture_station not in ('1','2','3'):
+            elif station_confirm not in ('Y','y'):
+                capture_station = ''
+                if not capture_station == '1' or capture_station == '2' or capture_station == '3':
                     capture_station =  raw_input('\n\n**** Where was tape captured?\nPress 1, 2 or 3\n\n1. es2\n2. loopline\n3. ingest 1\n' )
-            if capture_station == '1':
-                capture_station = 'es2'
-            elif capture_station == '2':
-                capture_station = 'loopline'
-            elif capture_station == '3':
-                capture_station = 'ingest1'
-            return capture_station
+                    while capture_station not in ('1','2','3'):
+                        capture_station =  raw_input('\n\n**** Where was tape captured?\nPress 1, 2 or 3\n\n1. es2\n2. loopline\n3. ingest 1\n' )
+                if capture_station == '1':
+                    capture_station = 'es2'
+                elif capture_station == '2':
+                    capture_station = 'loopline'
+                elif capture_station == '3':
+                    capture_station = 'ingest1'
+    return capture_station
 
 
 def main():
