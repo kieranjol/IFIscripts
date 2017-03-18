583a7b0bff19265d048315b7b53775b05bbc4600
kieranjol <kieran.o.leary@gmail.com>
2017-03-18 15:31:00 +0000

    masscopy - analyzes logs for summary report

--- masscopy.py
+++ masscopy.py
@@ -3,6 +3,21 @@ import sys
 import os
 import subprocess
 import argparse
+import time
+from ififuncs import make_desktop_logs_dir
+
+
+def analyze_log(logfile):
+    outcome = ''
+    with open(logfile, 'r') as fo:
+        log_lines = fo.readlines()
+        for line in log_lines:
+            if 'EVENT = File Transfer Judgement - Success ' in line:
+                outcome = 'success'
+            if 'EVENT = File Transfer Outcome - Failure' in line:
+                outcome = 'failure'
+        return outcome
+
 
 def main():
     parser = argparse.ArgumentParser(description='Performs moveit.py in a batch'
@@ -18,8 +33,12 @@ def main():
     dirlist = []
     permission = ''
     for source_directory in os.listdir(args.input):
-        if os.path.isdir(os.path.join(args.input,source_directory)):
-            manifest = os.path.join(args.input,source_directory) + '_manifest.md5'
+        if os.path.isdir(
+            os.path.join(args.input,source_directory)
+            ):
+            manifest = os.path.join(
+                args.input,source_directory
+                ) + '_manifest.md5'
             if os.path.isfile(manifest):
                 dirlist.append(os.path.join(args.input, source_directory))
     all_files = dirlist
@@ -35,14 +54,41 @@ def main():
             sys.exit()
         elif permission =='y' or permission == 'Y':
             print 'Ok so!'
+    processed_dirs = []
+    log_names = []
     for i in all_files:
-            if os.path.isdir(os.path.join(args.o, os.path.basename(i))):
-                print('%s already exists, skipping') % (os.path.join(args.o, os.path.basename(i)))
-            else:
-                subprocess.check_call([sys.executable,os.path.expanduser("~/ifigit/ifiscripts/moveit.py"),
-                os.path.join(args.input,i), args.o])
-                print '********\nWARNING - Please check the ifiscripts_logs directory on your Desktop to verify if ALL of your transfers were successful'
-
-
+        if os.path.isdir(
+            os.path.join(args.o, os.path.basename(i))
+            ):
+            print(
+                '%s already exists, skipping'
+                 ) % (os.path.join(args.o, os.path.basename(i)))
+        else:
+            log_name_source_ = os.path.basename(
+                os.path.join(args.input,i)
+                ) + time.strftime("_%Y_%m_%dT%H_%M_%S")
+            desktop_logs_dir = make_desktop_logs_dir()
+            log_name_source = "%s/%s.log" % (desktop_logs_dir, log_name_source_)
+            moveit_cmd = [
+                sys.executable,
+                os.path.expanduser("~/ifigit/ifiscripts/moveit.py"),
+                os.path.join(args.input,i), args.o]
+            subprocess.check_call(moveit_cmd)
+            processed_dirs.append(os.path.basename(os.path.join(args.input,i)))
+            log_names.append(log_name_source)
+            print '********\nWARNING - Please check the ifiscripts_logs directory on your Desktop to verify if ALL of your transfers were successful'
+    print 'SUMMARY REPORT'
+    for i in log_names:
+        if os.path.isfile(i):
+            print "%-*s   : %s" % (50,os.path.basename(i)[:-24], analyze_log(i))
+        else:
+            print i, 'can\'t find log file, trying again...'
+            for logs in os.listdir(desktop_logs_dir):
+                # look at log filename minus the seconds and '.log'
+                if os.path.basename(i)[:-7] in logs:
+                    # make sure that the alternate log filename is more recent
+                    if int(os.path.basename(logs)[-12:-4].replace('_','')) > int(os.path.basename(i)[-12:-4].replace('_','')):
+                        print 'trying to analyze %s' % logs
+                        print "%-*s   : %s" % (50,os.path.basename(logs)[:-24], analyze_log(os.path.join(desktop_logs_dir,logs)))
 if __name__ == '__main__':
     main()
