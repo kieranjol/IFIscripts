961ae36296bc583ba098944710eccb60dcbdae93
kieranjol <kieran.o.leary@gmail.com>
2017-04-01 21:02:20 +0100

    Revert "testfiles- cleanup & adds test_files subdirectory"
    This reverts commit ce781a38cf0db10c3373b8a349f0cbb4f2ab8850.

--- testfiles.py
+++ testfiles.py
@@ -1,33 +1,9 @@
 #!/usr/bin/env python
 import sys
 import subprocess
-import os
 
+input_dir = sys.argv[1]
 
-def main():
-    '''
-    Creates three v210/mov tesfiles in a test_files subdirectory
-    '''
-    output_dir = os.path.join(sys.argv[1], 'test_files')
-    bars_cmd = [
-        'ffmpeg', '-f', 'lavfi', '-i', 'testsrc',
-        '-c:v', 'v210', '-t', '20', os.path.join(output_dir, 'bars.mov')
-        ]
-    mandel_cmd = [
-        'ffmpeg', '-f', 'lavfi', '-i', 'mandelbrot',
-        '-c:v', 'v210', '-t', '20', os.path.join(output_dir, 'mandel.mov')
-        ]
-    life_cmd = [
-        'ffmpeg', '-f', 'lavfi', '-i', 'life',
-        '-c:v', 'v210', '-t', '20', os.path.join(output_dir, 'life.mov')
-        ]
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
-    subprocess.call(bars_cmd)
-    subprocess.call(mandel_cmd)
-    subprocess.call(life_cmd)
-
-if __name__ == '__main__':
-    main()
-
-
+subprocess.call(['ffmpeg', '-f', 'lavfi', '-i', 'testsrc', '-c:v', 'prores', '-t', '20', input_dir + 'bars.mov'])
+subprocess.call(['ffmpeg', '-f', 'lavfi', '-i', 'mandelbrot', '-c:v', 'prores', '-t', '20', input_dir + 'mandel.mov'])
+subprocess.call(['ffmpeg', '-f', 'lavfi', '-i', 'life', '-c:v', 'prores', '-t', '20', input_dir + 'life.mov'])
