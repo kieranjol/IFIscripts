08c362a042ad6ba8b7beb522359748ab437fdf57
kieranjol <kieran.o.leary@gmail.com>
2017-06-27 14:08:12 +0100

    sipcreator - adds manifest consolidation

--- sipcreator.py
+++ sipcreator.py
@@ -6,6 +6,7 @@ import os
 import argparse
 import ififuncs
 import sys
+import shutil
 import subprocess
 
 
@@ -36,6 +37,24 @@ def extract_checksums(manifest, path_list, basename):
     with open(manifest_textfile,"wb") as fo:
         for i in new_manifest:
             fo.write(i + '\n')
+
+def consolidate_manifests(path):
+    uuid = os.path.basename(path)
+    objects_dir = os.path.join(path, 'objects')
+    new_manifest_textfile = os.path.join(os.path.dirname(path), uuid + '_manifest.md5')
+    collective_manifest = []
+    for manifests in os.listdir(objects_dir):
+        if manifests.endswith('.md5'):
+             if not manifests[0] == '.':
+                 with open(os.path.join(objects_dir, manifests), 'r') as fo:
+                     manifest_lines = fo.readlines()
+                     for i in manifest_lines:
+                         new_manifest_path = uuid + '/objects/' + i[34:]
+                         collective_manifest.append(i[:32] + '  ' + new_manifest_path   )
+                 shutil.move(objects_dir + '/' +  manifests, os.path.join(path, 'logs'))
+    with open(new_manifest_textfile, 'ab') as manifest_object:
+        for checksums in collective_manifest:
+            manifest_object.write(checksums)
 
 
 def get_relative_filepaths(inputs):
@@ -69,7 +88,7 @@ def main():
     args = parser.parse_args()
     sip_path = make_folder_path(os.path.join(args.o))
     path_list = get_relative_filepaths(args.i)
-    print sip_path
+    
     '''
     print args.i
     basename = os.path.basename(args.i[0])
@@ -84,11 +103,12 @@ def main():
                         os.path.expanduser("~/ifigit/ifiscripts/moveit.py"),
                         t, os.path.join(sip_path, 'objects')]
         subprocess.check_call(moveit_cmd)
-        
-        '''
-        bring ubunutu and macbook to loopline,do transfers there
-        take manifests, add uuid, put in correct location, delete orig manifests
-        '''
+    new_manifest = consolidate_manifests(sip_path)
+    print sip_path
+    '''
+    bring ubunutu and macbook to loopline,do transfers there
+    take manifests, add uuid, put in correct location, delete orig manifests
+    '''
 
 if __name__ == '__main__':
     main()
