b8c3e9d6b0baeacfa355805d66b59c2156804ced
kieranjol <kieran.o.leary@gmail.com>
2017-07-12 17:53:28 +0100

    validate - switch logging to generate_log()

--- validate.py
+++ validate.py
@@ -2,10 +2,8 @@
 import sys
 import hashlib
 import os
-import logging
 import argparse
 import time
-import getpass
 import ififuncs
 from ififuncs import make_desktop_logs_dir
 
@@ -40,7 +38,7 @@ def get_input(manifest):
     else:
         return manifest
 
-def parse_manifest(manifest):
+def parse_manifest(manifest, log_name_source):
     missing_files = 0
     manifest_dict = {}
     os.chdir(os.path.dirname(manifest))
@@ -51,23 +49,33 @@ def parse_manifest(manifest):
             path = entries[34:].replace('\r', '').replace('\n', '')
             path = path.replace('\\', '/')
             if not os.path.isfile(path):
-                logging.info('%s is missing' % path )
+                ififuncs.generate_log(
+                    log_name_source,
+                    '%s is missing' % path 
+                )
                 print '%s is missing' % path
                 missing_files += 1
             elif os.path.isfile(path):
-                logging.debug('%s %s' % (checksum, path))
                 manifest_dict[path] = checksum
-                logging.debug(manifest_dict)
     if missing_files > 0:
         print 'The number of missing files: %s' % missing_files
-        logging.info('The number of missing files is: %s' %  missing_files)
+        ififuncs.generate_log(
+            log_name_source,
+            'The number of missing files is: %s' %  missing_files
+        )
     elif missing_files == 0:
         print 'All files present'
-        logging.info('All files present')
+        ififuncs.generate_log(
+            log_name_source,
+            'All files present'
+        )
     return manifest_dict, missing_files
 
-def validate(manifest_dict, manifest,missing_files):
-    logging.info('Validating %s ' % manifest)
+def validate(manifest_dict, manifest,missing_files, log_name_source):
+    ififuncs.generate_log(
+        log_name_source,
+        'Validating %s ' % manifest
+    )
     error_counter = 0
     manifest_directory = os.path.dirname(manifest)
     os.chdir(manifest_directory)
@@ -80,22 +88,35 @@ def validate(manifest_dict, manifest,missing_files):
             print '%s has validated' % i
         else:
             print '%s has mismatched checksum - %s expected - %s hashed' % (i, manifest_dict[i], current_hash)
-            logging.info('%s has mismatched checksum - %s expected - %s hashed' % (i, manifest_dict[i], current_hash))
+            ififuncs.generate_log(
+                log_name_source,
+                '%s has mismatched checksum - %s expected - %s hashed' % (i, manifest_dict[i], current_hash)
+            )
             error_list.append('%s has mismatched checksum - %s expected - %s hashed' % (i, manifest_dict[i], current_hash))
             error_counter += 1
     if error_counter > 0:
         print '\n\n*****ERRORS***********!!!!\n***********\nThe number of mismatched checksums is: %s\n***********\n' % error_counter
-        logging.info('The number of mismatched checksums is: %s' %  error_counter)
+        ififuncs.generate_log(
+        log_name_source,
+        'The number of mismatched checksums is: %s' %  error_counter
+    )
         print '***** List of mismatched files*****'
         for i in error_list:
             print i
     elif error_counter == 0:
         if missing_files > 0:
-            print '\n*****ERRORS!!!!***********\nThe number of missing files: %s\n***********\n' % missing_files
-            logging.info('\n***********ERRORS!!!!*****\nThe number of mismatched checksums is: %s\n***********\n' %  missing_files)
+            print 'ERRORS - The number of missing files: %s' % missing_files
+            ififuncs.generate_log(
+                log_name_source,
+                'ERRORS - The number of mismatched checksums is: %s' %  missing_files
+            )
+
         elif missing_files == 0:
             print 'All checksums have validated'
-            logging.info('All checksums have validated')
+            ififuncs.generate_log(
+                log_name_source,
+                'All checksums have validated'
+            )
 
 def make_parser():
     parser = argparse.ArgumentParser(description='MD5 checksum manifest validator. Currently this script expects an md5 checksum, followed by two spaces, followed by a file path.'
@@ -103,10 +124,10 @@ def make_parser():
     parser.add_argument('input', help='file path of md5 checksum file')
     return parser
 
-def check_manifest(input):
+def check_manifest(input, log_name_source):
     manifest = get_input(input)
-    manifest_dict, missing_files = parse_manifest(manifest)
-    validate(manifest_dict, manifest, missing_files)
+    manifest_dict, missing_files = parse_manifest(manifest, log_name_source)
+    validate(manifest_dict, manifest, missing_files, log_name_source)
     return manifest
 def log_results(manifest, log, args):
     updated_manifest = []
@@ -137,12 +158,20 @@ def main():
     args = parser.parse_args()
     desktop_logs_dir = make_desktop_logs_dir()
     log_name_source_ = os.path.basename(args.input) + time.strftime("_%Y_%m_%dT%H_%M_%S")
-    log = "%s/%s_fixity_validation.log" % (desktop_logs_dir, log_name_source_)
-    root = logging.getLogger()
-    logging.basicConfig(filename=log, filemode='a',level=logging.INFO)
-    logging.info('Started at %s using the following workstation: %s' % (time.strftime("%Y-%m-%dT%H:%M:%S "), getpass.getuser()))
-    #root.setLevel(logging.DEBUG)
-    manifest = check_manifest(args.input)
-    log_results(manifest, log, args)
+    log_name_source = "%s/%s_fixity_validation.log" % (desktop_logs_dir, log_name_source_)
+    ififuncs.generate_log(
+        log_name_source,
+        'EVENT = validate.py started'
+    )
+    ififuncs.generate_log(
+        log_name_source,
+        'eventDetail=validate.py %s' % ififuncs.get_script_version('validate.py')
+    )
+    ififuncs.generate_log(
+        log_name_source,
+        'Command line arguments: %s' % args
+    )
+    manifest = check_manifest(args.input, log_name_source)
+    log_results(manifest, log_name_source, args)
 if __name__ == '__main__':
    main()
