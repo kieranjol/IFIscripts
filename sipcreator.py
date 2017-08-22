b0dcf2a2f20b2141b098dc6491bf8fe40a4fe8ec
kieranjol <kieran.o.leary@gmail.com>
2017-08-22 20:41:17 +0100

    sipcreator - fixes bug where metadata is generated for mediainfo/mediatrace

--- sipcreator.py
+++ sipcreator.py
@@ -210,45 +210,47 @@ def get_metadata(path, new_log_textfile):
             elif av_file.endswith(
                     ('.tif', 'tiff', '.doc', '.txt', '.docx', '.pdf', '.jpg', '.jpeg', '.png', '.rtf', '.xml', '.odt')
             ):
+                blacklist = ('siegfried', 'exiftool', 'mediainfo', 'mediatrace')
                 if av_file[0] != '.':
-                    exiftool_version = 'exiftool'
-                    try:
-                        exiftool_version = subprocess.check_output([
-                            'exiftool', '-ver'
-                        ])
-                    except subprocess.CalledProcessError as grepexc:
-                        exiftool_version = grepexc.output.rstrip().splitlines()[1]
-                    siegfried_version = 'siegfried'
-                    try:
-                        siegfried_version = subprocess.check_output([
-                            'sf', '-version'
-                        ])
-                    except subprocess.CalledProcessError as grepexc:
-                        siegfried_version = grepexc.output.rstrip().splitlines()[1]
-                    inputxml = "%s/%s_exiftool.xml" % (
-                        os.path.join(path, 'metadata'), os.path.basename(av_file)
+                    if any(word in blacklist for word in av_file):
+                        exiftool_version = 'exiftool'
+                        try:
+                            exiftool_version = subprocess.check_output([
+                                'exiftool', '-ver'
+                            ])
+                        except subprocess.CalledProcessError as grepexc:
+                            exiftool_version = grepexc.output.rstrip().splitlines()[1]
+                        siegfried_version = 'siegfried'
+                        try:
+                            siegfried_version = subprocess.check_output([
+                                'sf', '-version'
+                            ])
+                        except subprocess.CalledProcessError as grepexc:
+                            siegfried_version = grepexc.output.rstrip().splitlines()[1]
+                        inputxml = "%s/%s_exiftool.xml" % (
+                            os.path.join(path, 'metadata'), os.path.basename(av_file)
+                            )
+                        inputtracexml = "%s/%s_siegfried.json" % (
+                            os.path.join(path, 'metadata'), os.path.basename(av_file)
+                            )
+                        ififuncs.make_siegfried(
+                            inputtracexml,
+                            os.path.join(root, av_file)
                         )
-                    inputtracexml = "%s/%s_siegfried.json" % (
-                        os.path.join(path, 'metadata'), os.path.basename(av_file)
+                        print 'Generating exiftool xml of input file and saving it in %s' % inputxml
+                        ififuncs.generate_log(
+                            new_log_textfile,
+                            'EVENT = Metadata extraction - eventDetail=Technical metadata extraction via exiftool, eventOutcome=%s, agentName=%s' % (inputxml, exiftool_version)
                         )
-                    ififuncs.make_siegfried(
-                        inputtracexml,
-                        os.path.join(root, av_file)
-                    )
-                    print 'Generating exiftool xml of input file and saving it in %s' % inputxml
-                    ififuncs.generate_log(
-                        new_log_textfile,
-                        'EVENT = Metadata extraction - eventDetail=Technical metadata extraction via exiftool, eventOutcome=%s, agentName=%s' % (inputxml, exiftool_version)
-                    )
-                    print 'Generating mediatrace xml of input file and saving it in %s' % inputtracexml
-                    ififuncs.make_exiftool(
-                        inputxml,
-                        os.path.join(root, av_file)
-                    )
-                    ififuncs.generate_log(
-                        new_log_textfile,
-                        'EVENT = Format identification - eventType=format identification, eventDetail=Format identification via PRONOM signatures using Siegfried, eventOutcome=%s, agentName=%s' % (inputtracexml, siegfried_version)
-                    )
+                        print 'Generating mediatrace xml of input file and saving it in %s' % inputtracexml
+                        ififuncs.make_exiftool(
+                            inputxml,
+                            os.path.join(root, av_file)
+                        )
+                        ififuncs.generate_log(
+                            new_log_textfile,
+                            'EVENT = Format identification - eventType=format identification, eventDetail=Format identification via PRONOM signatures using Siegfried, eventOutcome=%s, agentName=%s' % (inputtracexml, siegfried_version)
+                        )
 def create_content_title_text(sip_path):
     '''
     DCPs are often delivered with inconsistent foldernames.
