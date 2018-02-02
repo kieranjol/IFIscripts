e1b0c084fed4a7981efd364a8c7217d0a42b6b6f
kieranjol <kieran.o.leary@gmail.com>
2018-02-02 15:59:26 +0000

    accession - more interviews and sip checks

--- accession.py
+++ accession.py
@@ -7,6 +7,7 @@ MIT License
 '''
 
 import sys
+import os
 import argparse
 import ififuncs
 
@@ -15,7 +16,10 @@ def main():
     '''
     Launches the various functions that will accession a package
     '''
+    input = [sys.argv[1]]
+    oe_number =  os.path.basename(os.path.dirname(ififuncs.check_for_sip(input)))
     accession_number = ififuncs.get_accession_number()
+    ififuncs.ask_yes_no('Do you want to rename %s with %s' % ( oe_number, accession_number))
     
 if __name__ == '__main__':
     main()
