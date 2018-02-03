89f8f330c9091758f31044b95a98e25ae3d1a2d8
kieranjol <kieran.o.leary@gmail.com>
2018-02-03 17:31:32 +0000

    accession - adds better interview, input checks and renaming

--- accession.py
+++ accession.py
@@ -8,7 +8,6 @@ MIT License
 
 import sys
 import os
-import argparse
 import ififuncs
 
 
@@ -17,9 +16,21 @@ def main():
     Launches the various functions that will accession a package
     '''
     input = [sys.argv[1]]
-    oe_number =  os.path.basename(os.path.dirname(ififuncs.check_for_sip(input)))
-    accession_number = ififuncs.get_accession_number()
-    ififuncs.ask_yes_no('Do you want to rename %s with %s' % ( oe_number, accession_number))
-    
+    uuid_directory = ififuncs.check_for_sip(input)
+    if uuid_directory is not None:
+        oe_path = os.path.dirname(uuid_directory)
+        oe_number = os.path.basename(oe_path)
+        accession_number = ififuncs.get_accession_number()
+        accession_path = os.path.join(
+            os.path.dirname(oe_path), accession_number
+        )
+        proceed = ififuncs.ask_yes_no(
+            'Do you want to rename %s with %s' % (oe_number, accession_number)
+        )
+        if proceed == 'Y':
+            os.rename(oe_path, accession_path)
+        print oe_path, accession_path
+    else:
+        print 'not a valid package. The input should include a package that has been through Object Entry'
 if __name__ == '__main__':
     main()
