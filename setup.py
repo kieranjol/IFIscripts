387de244df10efef880611ee14d3edf14800e446
kieranjol <kieran.o.leary@gmail.com>
2018-04-06 23:41:20 +0100

    adds

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    author='Kieran O\'Leary',
+    author_email='kieran.o.leary@gmail.com',
+    description='IFIscripts',
+    scripts=['sipcreator.py', 'ififuncs.py', 'copyit.py', 'masscopy.py', 'accession.py'],
+    license='MIT',
+    name='ifiscripts',
+    version='0.4'
+)
