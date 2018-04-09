0005f7e3cfdcd86439a3d5cff73bba1737bf959a
kieranjol <kieran.o.leary@gmail.com>
2018-04-09 13:00:17 +0100

    order - adds interview when package ith single object but no parent is found

--- order.py
+++ order.py
@@ -9,6 +9,17 @@ import sys
 import os
 import ififuncs
 
+def file_count(source):
+    '''
+    Checks how many mov/mp4 files are in a dir.
+    '''
+    count = 0
+    for root, _, filenames in os.walk(source):
+        for files in filenames:
+            if files.lower().endswith(('.mov', '.mp4')):
+                count += 1
+    return count
+
 
 def main(args):
     '''
@@ -25,7 +36,17 @@ def main(args):
                         os.path.join(root, filename), oe_uuid_dict
                     )
                     if 'not a child' in uuid_search:
-                        return None
+                        # Checks if a single AV file is in the objects dir.
+                        uuid_dir = os.path.join(os.path.dirname(root))
+                        print root
+                        if file_count(os.path.join(uuid_dir, 'objects')) == 1:
+                            print '%s has no parent but this could be because it is a single file' % root
+                            proceed = ififuncs.ask_yes_no('add %s to accession list?' % root)
+                            if proceed == 'Y':
+                                print os.path.basename(os.path.dirname(uuid_dir))
+                                return os.path.basename(os.path.dirname(uuid_dir))
+                        else:
+                            return None
                     elif 'has a parent' in uuid_search:
                         parent = uuid_search[-7:-1]
                         print parent[:2].upper() + '-' + parent[2:]
