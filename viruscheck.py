4ba22a27fcc888346b4fd2bfb328cb1bc910cac7
kieranjol <kieran.o.leary@gmail.com>
2018-04-19 12:25:30 +0100

    viruscheck - adds verbose option, PEP-08 cleanup

--- viruscheck.py
+++ viruscheck.py
@@ -1,26 +1,50 @@
 #!/usr/bin/env python
-
-#Requires ClamAV to be installed
+'''
+Requires ClamAV to be installed
+'''
 
 import sys
 import subprocess
+import argparse
+
+def parse_args(args_):
+    '''
+    Parse command line arguments.
+    '''
+    parser = argparse.ArgumentParser(
+        description='Runs a virus scan using ClamAV on your input directory'
+        ' Written by Eoin O\'Donohoe.'
+    )
+    parser.add_argument(
+        'input', help='Input directory'
+    )
+    parsed_args = parser.parse_args(args_)
+    return parsed_args
 
 
-
-def clamscan():
+def clamscan(input_dir):
+    '''
+    Calls ClamAV.
+    '''
     scan = subprocess.call([
         'clamscan',
         '-r',
-        starting_dir
+        '-v',
+        input_dir
     ])
-    
-    print scan
-    
 
-starting_dir = sys.argv[1]
+def main(args_):
+    '''
+    Launches functions that performs a virus check.
+    '''
+    args = parse_args(args_)
+    input_dir = args.input
+    print "Running scan.........."
+    clamscan(input_dir)
 
-print "Running scan.........."
-clamscan()
+
+if __name__ == '__main__':
+    main(sys.argv[1:]) 
 
 
 
