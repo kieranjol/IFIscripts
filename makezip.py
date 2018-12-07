12348bb6a73fba60dd1abc1b6a769d20d5d1036d
kieranjol <kieran.o.leary@gmail.com>
2018-12-07 11:30:56 +0000

    makezip - uses general ifiscripts syntax

--- makezip.py
+++ makezip.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+'''
+Zips and verifies all files and folders within your input directory.
+'''
 from zipfile import ZipFile
 import zipfile
 import sys
@@ -8,7 +11,7 @@ import argparse
 import datetime
 
 
-def parse_args():
+def parse_args(args_):
     '''
     Parse command line arguments.
     '''
@@ -17,44 +20,47 @@ def parse_args():
         ' Written by Kieran O\'Leary.'
     )
     parser.add_argument(
-        'input', help='Input directory'
+        '-i', help='Input directory', required=True
     )
     parser.add_argument(
-        'destination', help='Output directory'
+        '-o', help='Output directory', required=True
     )
-    parsed_args = parser.parse_args()
+    parsed_args = parser.parse_args(args_)
     return parsed_args
 
-def main():
-    args = parse_args()
+def main(args_):
+    '''
+    Zips and verifies all files and folders within your input directory.
+    '''
+    args = parse_args(args_)
     pwd = os.getcwd()
-    source = args.input
-    destination = args.destination
-    # destination = sys.argv[2]
+    source = args.i
+    destination = args.o
     start = datetime.datetime.now()
     name = os.path.basename(source) + '.zip'
     full_zip = os.path.join(destination, name)
-    with ZipFile(full_zip, 'w',zipfile.ZIP_STORED, allowZip64 = True) as myzip:
+    with ZipFile(full_zip, 'w', zipfile.ZIP_STORED, allowZip64=True) as myzip:
         os.chdir(source)
-        for root, dirnames, filenames in os.walk(source):
+        for root, _, filenames in os.walk(source):
             for filename in filenames:
                 full_path = os.path.relpath(os.path.join(root, filename))
                 print('zipping %s' % full_path)
                 myzip.write(full_path)
     finish = datetime.datetime.now()
-    print start, finish           
+    print(start, finish)
     os.chdir(pwd)
     start = datetime.datetime.now()
     time.sleep(5)
     with zipfile.ZipFile(full_zip, 'r') as myzip:
-        print 'verifying..'
-        result =  myzip.testzip()
+        print('verifying..')
+        result = myzip.testzip()
         if result is None:
             print('Python has not detected any errors in your zipfile')
         else:
             print('ERROR DETECTED IN %s '% result)
     finish = datetime.datetime.now()
-    # print start, finish           
-    
+    print(start, finish)
+    return result, full_zip
+
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
