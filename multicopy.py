e22d773aeb15267fc9713a3488fb8dbe54285146
kieranjol <kieran.o.leary@gmail.com>
2018-12-26 12:12:26 +0000

    multicopy adds -y and -l options, fixes #309

--- multicopy.py
+++ multicopy.py
@@ -34,6 +34,11 @@ def parse_args():
         '-l', '-lto',
         action='store_true',
         help='use gcp instead of rsync on osx for SPEED on LTO')
+    parser.add_argument(
+        '-y',
+        action='store_true',
+        help='Answers YES to the question: Not enough free space, would you like to continue?'
+    )
     args = parser.parse_args()
     return args
 
@@ -46,7 +51,12 @@ def main():
     args = parse_args()
     desktop_logs_dir = ififuncs.make_desktop_logs_dir()
     for i in args.i:
-        log_names.append(copyit.main([i, args.o]))
+        copyit_cmd = [i, args.o]
+        if args.l:
+            copyit_cmd.append('-l')
+        elif args.y:
+            copyit_cmd.append('-y')
+        log_names.append(copyit.main(copyit_cmd))
     print '********\nWARNING - Please check the ifiscripts_logs directory on your Desktop to verify if ALL of your transfers were successful'
     masscopy.analyze_reports(log_names, desktop_logs_dir)
 
