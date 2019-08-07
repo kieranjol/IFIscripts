b68aee50637defe46b34a98abf2d204c187589b7
kieranjol <kieran.o.leary@gmail.com>
2019-08-07 14:51:15 +0100

    mergecsv - adds check for CSV files with no values

--- mergecsv.py
+++ mergecsv.py
@@ -19,11 +19,14 @@ def gather_metadata(source):
             if filename.endswith('csv'):
                 with open(os.path.join(root,filename), 'r') as csv_file:
                     csv_rows = csv_file.readlines()
-                if metadata:
-                    metadata.append([csv_rows[1]])
+                if len(csv_rows) > 1:
+                    if metadata:
+                        metadata.append([csv_rows[1]])
+                    else:
+                        metadata.append([csv_rows[0]])
+                        metadata.append([csv_rows[1]])
                 else:
-                    metadata.append([csv_rows[0]])
-                    metadata.append([csv_rows[1]])
+                    print('%s has been skipped due to no actual data in the CSV' % filename)
     collated_pbcore = os.path.join(
         ififuncs.make_desktop_logs_dir(),
         time.strftime("%Y-%m-%dT%H_%M_%S_collated.csv")
