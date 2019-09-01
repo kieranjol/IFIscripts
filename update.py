75000898aa78242afc67fcc5cab71b9b3e6debe9
kieranjol <kieran.o.leary@gmail.com>
2019-09-01 17:25:56 +0100

    update - adds python3 support

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import subprocess
 import sys
@@ -9,7 +9,7 @@ def install_check(directory):
     if os.path.isdir(directory):
         return True
     else:
-        print('\nWARNING - %s does not exist!\nCreate a directory in your home directory called `ifigit` and git clone the repository in there') % directory
+        print('\nWARNING - %s does not exist!\nCreate a directory in your home directory called `ifigit` and git clone the repository in there' % directory)
         return False
 
 def main():
@@ -17,15 +17,15 @@ def main():
     os.chdir(home)
     if install_check('ifigit/ifiscripts'):
         os.chdir('ifigit/ifiscripts')
-        print('Updating IFIScripts - Changing directory to %s and running `git pull`') %os.getcwd()
+        print("Updating IFIScripts - Changing directory to %s and running `git pull`" % os.getcwd())
         subprocess.call(['git', 'checkout', 'master'])
         subprocess.call(['git', 'pull'])
     if install_check('../premisviewer'):
         os.chdir('../premisviewer')
-        print('Updating PREMISviewer - Changing directory to %s and running `git pull`') %os.getcwd()
+        print('Updating PREMISviewer - Changing directory to %s and running `git pull`' % os.getcwd())
         subprocess.call(['git', 'pull'])
     print('\nNotice - 2017-01-13')
-    print ('********** PREMISVIEWER is now part of update.py \n***************')
+    print('********** PREMISVIEWER is now part of update.py \n***************')
 
 if __name__ == '__main__':
     main()
