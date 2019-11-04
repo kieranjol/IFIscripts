e69ec1fb1c958ed6da353d2366490a1331e9958b
kieranjol <kieran.o.leary@gmail.com>
2019-11-04 09:56:02 +0000

    makezip - splits zips into 100gig files for greater LTO effeciency

--- makezip.py
+++ makezip.py
@@ -43,9 +43,9 @@ def create_zip(source, destination, name):
     full_zip = os.path.join(destination, name)
     os.chdir(os.path.dirname(source))
 
-    # check if input folder size is greater than 500 gigs 500000000000
-    if ififuncs.get_folder_size(source) > 500000000000:
-        subprocess.call(['7za', 'a', '-tzip', '-v500g', '-mx=0', full_zip, os.path.basename(source)])
+    # check if input folder size is greater than 100 gigs 100000000000
+    if ififuncs.get_folder_size(source) > 100000000000:
+        subprocess.call(['7za', 'a', '-tzip', '-v100g', '-mx=0', full_zip, os.path.basename(source)])
         full_zip = full_zip + '.001'
     else:
         subprocess.call(['7za', 'a', '-tzip', '-mx=0', full_zip, os.path.basename(source)])
