49f3aedb191f713b5b523c517c9ad6118a765e55
kieranjol <kieran.o.leary@gmail.com>
2020-04-03 17:05:57 +0100

    setup - updates to python 3.8

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+import sys
 from setuptools import setup
+
+if sys.version_info < (3, 8):
+    print("Python 3.8 or higher is required - earlier python 3 versions may work but were not tested.")
+    sys.exit(1)
 setup(
     author='Kieran O\'Leary',
     author_email='kieran.o.leary@gmail.com',
     description="Scripts for processing moving image material in the Irish Film Institute/Irish Film Archive",
     long_description=("""\
-Scripts for use in the IFI Irish Film Archive. Scripts have been tested in OSX/Windows 7 (sometimes windows 10) and Ubuntu 14.04. The aim is to make cross-platform scripts, but please get in touch with any issues. It is best to download all scripts, as some of them share code.
+Scripts for use in the IFI Irish Film Archive. Scripts have been tested in OSX/Windows 7/10 and Ubuntu 18.04. The aim is to make cross-platform scripts, but please get in touch with any issues. It is best to download all scripts, as some of them share code.
 
 Most scripts take either a file or a directory as their input, for example makeffv1.py filename.mov or premis.py path/to/folder_of_stuff. (It's best to just drag and drop the folder or filename into the terminal)
 
@@ -58,5 +64,6 @@ NOTE: Objects.py has been copied from https://github.com/simsong/dfxml. walk_to_
     data_files=[('', ['film_scan_aip_documentation.txt', '26_XYZ-22_Rec709.cube'])],
     include_package_data=True,
     name='ifiscripts',
-    version='v2018.12.25.3',
+    version='v2020.04.03',
+    python_requires='>=3.8'
 )
